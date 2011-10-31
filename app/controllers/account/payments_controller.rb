2d821e20393bca9fa3ca166e2f33e077f6cf9112
LAI KAM Lola <lailol@directmada.com>
2011-10-31 16:33:48 +0100

    correction de la logique et de bug

--- app/controllers/account/payments_controller.rb
+++ app/controllers/account/payments_controller.rb
@@ -4,7 +4,7 @@ class Account::PaymentsController < Account::AccountController
   skip_before_filter :verify_authenticity_token, :only => [:mode]
 public
   def mode
-    if params[:use_debit_mandate] == true && current_user.debit_mandate != nil
+    if params[:use_debit_mandate] == "true" && current_user.debit_mandate != nil
       current_user.use_debit_mandate = true if current_user.debit_mandate.transactionStatus == "success"
       current_user.save
     else
