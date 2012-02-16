1cb86217743217fcc76e0f5312e14e371c5d530c
LAI KAM Lola <lailol@directmada.com>
2012-02-16 18:13:43 +0100

    Module d'upload de fichier

--- app/controllers/account/documents/uploads_controller.rb
+++ app/controllers/account/documents/uploads_controller.rb
@@ -1,41 +1,50 @@
 class Account::Documents::UploadsController < Account::AccountController
+  helper :all
   
   skip_before_filter :verify_authenticity_token, :only => %w(create)
 
   def create
-    if params[:file] && [".pdf",".bmp",".jpeg",".jpg",".png","tiff",".tif",".gif"].include?(File.extname(params[:file].original_filename))
-      month = Time.now.month.to_s.length == 1 ? "0"+Time.now.month.to_s : Time.now.month.to_s
-      
-      type = Reporting.where(:client_ids => current_user.id).first.user.account_book_types.where(:name => params[:type]).first.name rescue nil
-      
-      if type
-        basename = current_user.code.to_s+"_"+type+"_"+Time.now.year.to_s+month
-        
-        Dir.chdir("#{Rails.root}/tmp/input_pdf_auto/ocr_tasks")
-        
-        checkpoint_number = Dir.entries('.').select{|d| d.match(/^#{basename}/)}.sort.last.split('_')[3].sub('.pdf','').to_i + 1 rescue 0
-        if checkpoint_number == 0
-          nb = Dir.entries('..').select{|d| d.match(/^#{basename}/)}.sort.last.split('_')[3].sub('.pdf','').to_i + 1 rescue 500
-          (nb < 500) ? checkpoint_number = 500 : checkpoint_number = nb
+    type = current_user.prescriber.account_book_types.where(:name => params[:account_book_type]).first.name rescue nil
+    
+    data = []
+    hsh = {}
+    
+    if type && !current_user.code.blank? && params[:files][0].original_filename && params[:files][0].tempfile
+      uploaded_file = current_user.uploaded_files.create(:original_filename => params[:files][0].original_filename, :account_book_type => params[:account_book_type])
+      if uploaded_file.persisted?
+        uploaded_file.moov_file params[:files][0].tempfile
+        if uploaded_file.is_password_protected?
+          uploaded_file.delete_file
+          uploaded_file.delete
+          hsh[:name] = params[:files][0].original_filename
+          hsh[:error] = "document protégé"
+          data << hsh
+        else
+          hsh[:created_at] = l(uploaded_file.created_at)
+          hsh[:name] = uploaded_file.original_filename
+          hsh[:new_name] = uploaded_file.pack_name.to_s + ".pdf"
+          data << hsh
         end
-        
-        file_name = basename + "_" + ("0"*(3 - checkpoint_number.to_s.length)) + checkpoint_number.to_s + File.extname(params[:file].original_filename)
-        
-        file = File.new(file_name,'w+')
-        FileUtils.copy_stream(params[:file].tempfile,file)
-        file.rewind
-
-        respond_to do |format|
-          format.json{ render :json => {:success => true, :file_name => file_name} }
-          # this one is for IE8 who is really dumb...
-          format.html{ render :json => {:success => true, :file_name => file_name} }
-        end
+      else
+        hsh[:name] = params[:files][0].original_filename
+        hsh[:error] = "document non valide"
+        data << hsh
+      end
+      
+      respond_to do |format|
+        format.json{ render :json => data }
+        # this one is for IE8 who is really dumb...
+        format.html{ render :json => data }
       end
     else
+      hsh[:name] = params[:files][0].original_filename
+      hsh[:error] = "document non valide"
+      data << hsh
+      
       respond_to do |format|
-        format.json{ render :json => {:success => false, :file_name => file_name} }
+        format.json{ render :json => data }
         # this one is for IE8 who is really dumb...
-        format.html{ render :json => {:success => false, :file_name => file_name} }
+        format.html{ render :json => data }
       end
     end
   end
---
+++ app/models/uploaded_file.rb
@@ -0,0 +1,106 @@
+class UploadedFile
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  VALID_EXTENSION = [".pdf",".bmp",".jpeg",".jpg",".png",".tiff",".tif",".gif"]
+  
+  after_create :do_process
+  
+  referenced_in :user
+  
+  field :basename, :type => String
+  field :account_book_type, :type => String
+  field :number, :type => String
+  field :original_filename, :type => String
+  field :is_ocr_needded, :type => Boolean, :default => true
+  field :is_delivered, :type => Boolean, :default => false
+  
+  validates_presence_of :original_filename, :account_book_type
+  
+  scope :valid, :where => { :is_valid => true }
+  scope :not_valid, :where => { :is_valid => false }
+  scope :delivered, :where => { :delivered => true }
+  scope :not_delivered, :where => { :delivered => false }
+  
+  validate :validity_of_extension
+  
+public
+  def processing_filename
+    "#{self.basename}_#{self.number}#{self.original_extension}"
+  end
+  
+  def pack_name
+    "#{self.basename}_all"
+  end
+  
+  def moov_file tempfile
+    Dir.chdir("#{Rails.root}/tmp/input_pdf_auto/ocr_tasks/")
+    new_name = "#{self.basename}_#{self.number}#{self.original_extension}"
+    file = File.new(new_name,'w+')
+    FileUtils.copy_stream(tempfile,file)
+    file.rewind
+  end
+  
+  def is_password_protected?
+    if self.original_extension == ".pdf"
+      # FIXME use more predicted code
+      !system("pdftk #{Rails.root}/tmp/input_pdf_auto/ocr_tasks/#{processing_filename} dump_data output /dev/null")
+    else
+      false
+    end
+  end
+  
+  def delete_file
+    system("rm #{Rails.root}/tmp/input_pdf_auto/ocr_tasks/#{processing_filename}")
+  end
+  
+  def do_process
+    set_basename
+    set_number
+    self.save
+  end
+
+  def set_basename
+    unless account_book_type.blank?
+      month = Time.now.month > 9 ? Time.now.month.to_s : "0"+Time.now.month.to_s
+      year = Time.now.year.to_s
+      self.basename = "#{self.user.code}_#{self.account_book_type}_#{year}#{month}"
+    end
+  end
+  
+  def set_number
+    nb = 0
+    if filename = get_last_similar_filename(".")
+      nb = filename.split('_')[3].sub('.pdf','').to_i + 1
+      if nb == 0
+        if filename = get_last_similar_filename("..")
+          nb = filename.split('_')[3].sub('.pdf','').to_i + 1
+          nb = 500 if nb < 500 && nb > 999
+        else
+          nb = 500
+        end
+      else
+        nb = 500 if nb < 500 && nb > 999
+      end
+    else
+    end
+    self.number = (1000+nb).to_s[1..3]
+  end
+  
+  def original_extension
+    File.extname(self.original_filename) rescue ""
+  end
+  
+  def get_last_similar_filename path
+    Dir.entries("#{Rails.root}/tmp/input_pdf_auto/ocr_tasks/#{path}").select{|d| d.match(/^#{self.basename}/)}.sort.last
+  end
+  
+  def validity_of_extension
+    unless VALID_EXTENSION.include?(original_extension.downcase)
+      errors.add(:original_extension, "Extension '#{original_extension}' is not valid, valid is : #{VALID_EXTENSION.join(' ')}")
+    else
+      true
+    end
+  end
+
+end
