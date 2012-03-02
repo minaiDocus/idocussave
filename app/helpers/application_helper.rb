ec5a37be53becd041c6263037e0af28c566501df
LAI KAM Lola <lailol@directmada.com>
2012-03-02 19:14:57 +0100

    Mise a jour des 'helpers'.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,6 +8,11 @@ module ApplicationHelper
     price_in_euros = price_in_cents.blank? ? "" : price_in_cents.round/100.0
     ("%0.2f" % price_in_euros).gsub(".", ",").gsub(/,00/, "")
   end
+  
+  def format_price_with_dot price_in_cents
+    price_in_euros = price_in_cents.blank? ? "" : price_in_cents.round/100.0
+    ("%0.2f" % price_in_euros).gsub(/.00/, "")
+  end
 
   def format_tiny_price price_in_cents
     price_in_euros = price_in_cents.blank? ? "" : price_in_cents/100.0
--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -21,9 +21,9 @@ module DocumentsHelper
   end
   
   def account_book_types_option
-    reporting = Reporting.where(:client_ids => current_user.id).first
-    if reporting
-      reporting.user.account_book_types.by_position.map do |u|
+    prescriber = current_user.prescriber
+    if prescriber
+      prescriber.account_book_types.by_position.map do |u|
         description = u.description.blank? ? "" : " " + u.description
         [u.name + description, u.name]
       end
@@ -35,4 +35,32 @@ module DocumentsHelper
   def tags_of document
     Iconv.iconv('ISO-8859-1', 'UTF-8', DocumentTag.where(:user_id => current_user.id, :document_id => document.id).first.name).join() rescue ""
   end
+  
+  def filter_list_of_users users
+    sorted_users = users.sort do |a,b|
+      if a.code != b.code
+        a.code <=> b.code
+      elsif a.company != b.company
+        a.company <=> b.company
+      elsif (a.first_name + " " + a.last_name) != (b.first_name + " " + b.last_name)
+        (a.first_name + " " + a.last_name) <=> (b.first_name + " " + b.last_name)
+      else
+        a.email <=> b.email
+      end
+    end
+    sorted_users.collect do |user|
+      name = user.code.blank? ? "" : user.company.blank? ? user.code : "#{user.code} - "
+      name += user.company.blank? ? "" : user.first_name.blank? ? user.company : "#{user.company} - "
+      name += user.first_name.blank? ? "" : user.last_name.blank? ? user.first_name : "#{user.first_name} #{user.last_name}"
+      [name, user.id]
+    end
+  end
+  
+  def format_user user
+    name = user.code.blank? ? "" : user.company.blank? ? user.code : "#{user.code} - "
+    name += user.company.blank? ? "" : user.first_name.blank? ? user.company : "#{user.company} - "
+    name += user.first_name.blank? ? "" : user.last_name.blank? ? user.first_name : "#{user.first_name} #{user.last_name}"
+    name
+  end
+  
 end
