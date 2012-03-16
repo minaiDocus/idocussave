84741fa179587c37db33deee303793e77eedf5b4
LAI KAM Lola <lailol@directmada.com>
2012-03-16 14:43:59 +0100

    Synchronisation du signale de traitement entre les requetes AJAX.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,18 +1,25 @@
+var ActionPerformCount = 0;
+
 $(document).ajaxSend(function(e, xhr, options) {
   var token = $("meta[name='csrf-token']").attr("content");
   xhr.setRequestHeader("X-CSRF-Token", token);
 });
 
 function logBeforeAction(msg) {
+  ActionPerformCount += 1;
   $(".feedback > .out").html(msg);
   $(".feedback").removeClass("inactive");
   $(".feedback").addClass("active");
 }
 
 function logAfterAction() {
-  $(".feedback > .out").html("Aucun traitement");
-  $(".feedback").addClass("inactive");
-  $(".feedback").removeClass("active");
+  ActionPerformCount -= 1;
+  if (ActionPerformCount <= 0) {
+    ActionPerformCount = 0;
+    $(".feedback > .out").html("Aucun traitement");
+    $(".feedback").addClass("inactive");
+    $(".feedback").removeClass("active");
+  }
 }
 
 function remove_fields(link) {
