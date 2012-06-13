3ce3a7ffe52be41f57f9508b4e3349b1c1527e9b
Lola LAI KAM <lailol@directmada.com>
2012-06-13 14:37:31 +0000

    Utilisation du gem 'exception_notification' a la place de 'airbrake', pour la gestion des erreurs, et amelioration graphique des pages d'erreurs 404 et 500.

--- app/controllers/account/account_controller.rb
+++ app/controllers/account/account_controller.rb
@@ -1,31 +1,34 @@
 class Account::AccountController < ApplicationController
   before_filter :authenticate_user!
-  #around_filter :catch_404
-
-  skip_before_filter :authenticate_super_user!
+  around_filter :catch_error if %w(staging production).include?(Rails.env)
   
   layout "inner"
-
+  
 protected
-
-  def catch_404
+  
+  def catch_error
     begin
       yield
-    rescue ActionController::UnknownAction, ActionController::RoutingError, BSON::InvalidObjectID, BSON::InvalidObjectId
-      render "/account/404", :status => 404
-    rescue Mongoid::Errors::DocumentNotFound => e
-      #@klass = t e.klass.to_s.downcase
-      render "/account/404", :status => 404
-    rescue
-      raise
+    rescue ActionController::RoutingError
+      redirect_to root_url
+    rescue  ActionController::UnknownController,
+                ActionController::UnknownAction,
+                BSON::InvalidObjectId,
+                Mongoid::Errors::DocumentNotFound
+      @page_types = PageType.by_position rescue []
+      @page_in_footer = Page.in_footer.visible.by_position rescue []
+      render :template => "404.html.haml", :status => 404, :layout => "inner"
+    rescue => e
+      ExceptionNotifier::Notifier.exception_notification(request.env, e).deliver
+      @page_types = PageType.by_position rescue []
+      @page_in_footer = Page.in_footer.visible.by_position rescue []
+      render :template => "500.html.haml", :status => 500, :layout => "inner"
     end
   end
-
+  
 public
-
+  
   def index
-
   end
-
-
+  
 end
