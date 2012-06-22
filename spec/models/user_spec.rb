20dba3b238fee2fa9aeede4390a8473a715025e1
Lola LAI KAM <lailol@directmada.com>
2012-06-22 09:10:32 +0000

    Integration du framework de test 'rspec' et ecriture des premiers test pour le systeme de gestion de l'abonnement.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "validate new user" do
+    user = User.new(:email => "test@example.com", :password => "secret")
+    user.should be_valid
+    user.skip_confirmation!
+    user.save
+    user.should be_persisted
+  end
 end
