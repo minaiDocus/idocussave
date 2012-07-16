b7728a6a35283f02abc5157d87c0677ff981b51e
Lola LAI KAM <lailol@directmada.com>
2012-07-16 14:06:22 +0000

    Execution des commandes via 'bundle exec'

--- app/models/background_process.rb
+++ app/models/background_process.rb
@@ -2,11 +2,11 @@ class BackgroundProcess
   ENV["RAILS_ENV"] = Rails.env
   
   def self.run
-    `lib/daemons/maintenance_ctl start`
+    `bundle exec lib/daemons/maintenance_ctl start`
   end
   
   def self.status
-    if `lib/daemons/maintenance_ctl status`.match(/no instances running/)
+    if `bundle exec lib/daemons/maintenance_ctl status`.match(/no instances running/)
       false
     else
       true
