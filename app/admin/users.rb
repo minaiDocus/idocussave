78c3ce143854d17ed920a05753e417b7a19a1a7e
Lola LAI KAM <lailol@directmada.com>
2012-07-31 19:14:43 +0300

    Ajout de fonctionnalité et correction graphique

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -9,11 +9,11 @@ ActiveAdmin.register User do
   filter :email, as: 'string', label: 'E-mail'
 
   index do
-    column 'Date de création', sortable: true do |user|
+    column 'Date de création', sortable: :created_at do |user|
       user.created_at
     end
     column :email
-    column 'Société / Code' do |user|
+    column 'Société / Code', sortable: :code do |user|
       [user.company, user.code].join(' ')
     end
     column 'Est confirmé ?' do |user|
@@ -22,20 +22,86 @@ ActiveAdmin.register User do
     default_actions
   end
 
-  show do |user|
-    attributes_table do
-      row :created_at
-      row :name
-      row :code
-      row :company
-      row 'Acitf ?' do |user|
-        user.inactive_at.nil? ? 'Oui' : 'Non'
+  show title: :name do |user|
+    div class: 'panel' do
+      h3 'Informations'
+      div class: 'panel_contents' do
+         div class: 'attributes_table user' do
+            table do
+              tbody do
+                tr do
+                  th 'Date d\'inscription'
+                  td user.created_at
+                end
+                tr do
+                  th 'Nom complet'
+                  td [user.first_name,user.last_name].join(' ')
+                end
+                tr do
+                  th 'Code client'
+                  td user.code
+                end
+                tr do
+                  th 'Société'
+                  td user.company
+                end
+                tr do
+                  th 'Actif ?'
+                  td user.inactive_at.nil? ? 'Oui' : 'Non'
+                end
+                tr do
+                  th 'Est administrateur ?'
+                  td user.is_admin ? 'Oui' : 'Non'
+                end
+                tr do
+                  th 'Est prescripteur ?'
+                  td user.is_prescriber ? 'Oui' : 'Non'
+                end
+                tr do
+                  th 'Mode de paiement'
+                  td user.use_debit_mandate ? 'Prélèvement' : 'Prépayé'
+                end
+              end
+            end
+         end
       end
-      row :is_admin
-      row :is_prescriber
-      row :use_debit_mandate
+    end
+
+    div class: 'panel' do
+      h3 'Adresses'
+      div class: 'panel_contents' do
+        div class: 'attributes_table user' do
+          table do
+            tbody do
+              tr do
+                th 'Facturation'
+                td render partial: 'address', locals: { address: user.billing_address }
+              end
+              tr do
+                th 'Retour'
+                td render partial: 'address', locals: { address: user.shipping_address }
+              end
+            end
+          end
+        end
+      end
     end
   end
 
-  form :partial => "form"
+  collection_action :search_by_code, :method => :get do
+    tags = []
+    if params[:q].present?
+      users = User.where(code: /.*#{params[:q]}.*/i)
+      users = users.prescribers if params[:prescriber].present?
+      users.each do |user|
+        tags << {id: user.id, name: user.code}
+      end
+    end
+
+    respond_to do |format|
+      format.json{ render json: tags.to_json, status: :ok }
+    end
+  end
+
+  form partial: "form"
 end
