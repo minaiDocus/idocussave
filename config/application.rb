c983bac09922df7a3b65e4c2075218c2287bba5c
Lola LAI KAM <lailol@directmada.com>
2012-08-07 12:42:04 +0300

    Langue par défaut fr

--- config/application.rb
+++ config/application.rb
@@ -48,6 +48,7 @@ module Idocus
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.locale = :fr
     config.i18n.default_locale = :fr
 
     # JavaScript files you want as :defaults (application.js is always included).
