71130555cff538f03d21026561698bccefe096ac
Lola LAI KAM <lailol@directmada.com>
2012-08-23 11:17:24 +0300

    Remove Rails 2.3-style plugins deprecation warning

--- config/initializers/paiementcic.rb
+++ config/initializers/paiementcic.rb
@@ -1,3 +1,6 @@
+require File.dirname(__FILE__) + '/../../lib/paiement_cic/lib/paiement_cic'
+require File.dirname(__FILE__) + '/../../lib/paiement_cic/lib/paiement_cic_helper'
+
 # here the hmac key calculated with the js calculator given by CIC
 PaiementCic.hmac_key = "3FA137460D973107A0F0C1E307A816EFDF83989B"
 # Here the TPE number
--- vendor/plugins/paiementcic/init.rb
+++
@@ -1,3 +0,0 @@
-Dir[File.join(File.dirname(__FILE__), 'lib/*.rb')].each { |file| load file }
-require "paiement_cic"
-require "paiement_cic_helper"
