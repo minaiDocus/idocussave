2490978399cf4f738c7c37d6b81b1bc83e5fd4dc
Lola LAI KAM <lailol@directmada.com>
2012-09-13 16:53:14 +0300

    dissociate contents dir for test mode

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,5 @@
 Paperclip.options[:command_path] = "/usr/bin"
-Paperclip::Attachment.default_options[:url] = "/system/:attachment/:id/:style/:filename"
+Paperclip::Attachment.default_options[:url] = "/system/:attachment/:id/:style/:filename" unless Rails.env.test?
+Paperclip::Attachment.default_options[:url] = "/system_test/:attachment/:id/:style/:filename" if Rails.env.test?
 Paperclip::Attachment.default_options[:path] = ":rails_root/public:url"
 Paperclip::Attachment.default_options[:storage] = :filesystem
