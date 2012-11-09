1335f870d990024e2bcb5bccd1c7b603c13cfe84
Lola LAI KAM <lailol@directmada.com>
2012-11-09 18:41:14 +0300

    restructuration du processus de regroupement des feuilles

--- lib/regroup_sheet.rb
+++ lib/regroup_sheet.rb
@@ -6,8 +6,8 @@ module RegroupSheet
 
   def self.processed_infos
     Dir.entries(RegroupSheet::INFOS_PATH).
-        select { |e| e.match(/_retour/) }.
-        map { |e| e.sub('_retour','') }
+        select { |e| e.match(/^\d{8}\.txt$/) }
+        map { |e| e.sub('txt','xml') }
   end
 
   def self.not_processed_infos
@@ -22,21 +22,18 @@ module RegroupSheet
       file = File.open(File.join(RegroupSheet::INFOS_PATH,filename))
       doc = Nokogiri::XML(file)
       doc.css('lot').each do |lot|
-        name = lot['name'].gsub('_',' ') + ' all'
-        pack = Pack.find_by_name(name)
-        if pack
-          lot.css('piece').each do |piece|
-            filespath = piece.css('sheet').map do |e|
-              filename = "#{lot['name']}_%0.3d.pdf" % e.content
-              File.join([RegroupSheet::CACHED_FILES_PATH,filename])
-            end.join(' ')
-            new_filename = "#{lot['name']}_#{'%0.3d' % piece['number']}.pdf"
-            filesname << new_filename
-            filepath = File.join([output_path,new_filename])
-            `pdftk #{filespath} cat output #{filepath}`
-          end
+        lot.css('piece').each do |piece|
+          filespath = piece.css('feuilles').map do |e|
+            filename = "#{lot['name']}_%0.3d.pdf" % e.content.to_i
+            File.join([RegroupSheet::CACHED_FILES_PATH,filename])
+          end.join(' ')
+          new_filename = "#{lot['name']}_#{'%0.3d' % piece['number']}.pdf"
+          filesname << new_filename
+          filepath = File.join([output_path,new_filename])
+          `pdftk #{filespath} cat output #{filepath}`
         end
       end
+      File.new(File.join(RegroupSheet::INFOS_PATH,filename.sub('.xml','.txt')))
     end
     filesname
   end
