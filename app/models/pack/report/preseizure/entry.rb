f8e9b00b855842a5a7b7a2b0c8f74e571724077e
Lola LAI KAM <lailol@directmada.com>
2012-11-12 13:49:43 +0300

    restructuration de la gestion des données de presaisie

---
+++ app/models/pack/report/preseizure/entry.rb
@@ -0,0 +1,32 @@
+# -*- encoding : UTF-8 -*-
+class Pack::Report::Preseizure::Entry
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  referenced_in :preseizure, class_name: 'Pack::Report::Preseizure'       , inverse_of: :entries
+  referenced_in :account   , class_name: 'Pack::Report::Preseizure::Entry', inverse_of: :entries
+
+  DEBIT  = 1
+  CREDIT = 2
+
+  field :type,   type: Integer
+  field :number, type: String
+  field :amount, type: Float
+
+  validates_presence_of :type
+  validates_presence_of :amount
+
+  alias :by_position :by_number
+
+  def get_debit
+    type == DEBIT ? amount : nil
+  end
+
+  def get_credit
+    type == CREDIT ? amount : nil
+  end
+
+  def self.by_number
+    asc(:type,:number)
+  end
+end
