8c6696b5a3fd68594b5d7d209eb7e22991d47c57
Lola LAI KAM <lailol@directmada.com>
2012-11-22 13:27:37 +0300

    correction problème d'autorisation d'accès

--- app/controllers/account/invoices_controller.rb
+++ app/controllers/account/invoices_controller.rb
@@ -1,13 +1,9 @@
 class Account::InvoicesController < Account::AccountController
-
-  before_filter :load_user
-
-  public
-
   def download
     invoice = Invoice.find params[:id]
     filepath = invoice.content.path params[:style]
-    if File.exist?(filepath) && (invoice.user == @user || invoice.user.try(:prescriber) == @user || @user.try(:is_admin))
+    debugger
+    if File.exist?(filepath) && (invoice.user == current_user || invoice.user.try(:prescriber) == current_user || current_user.is_admin)
       filename = File.basename filepath
       type = invoice.content_file_type || 'application/pdf'
       send_file(filepath, type: type, filename: filename, x_sendfile: true, disposition: 'inline')
