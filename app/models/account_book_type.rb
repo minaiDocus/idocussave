60cbb18c621f1f48d196ae7a82c6a569118494b0
Lola LAI KAM <lailol@directmada.com>
2013-04-10 11:48:21 +0300

    ajout du champ instructions aux journaux comptables, et affichage dans les kits d'envois de documents

--- app/models/account_book_type.rb
+++ app/models/account_book_type.rb
@@ -29,12 +29,14 @@ class AccountBookType
   field :vat_account,            type: String
   field :anomaly_account,        type: String
   field :is_default,             type: Boolean, default: false
+  field :instructions,            type: String
   
   slug :name
 
   validates_presence_of  :name
   validates_presence_of  :description
   validates_inclusion_of :entry_type, in: 0..3
+  validates_length_of    :instructions, maximum: 400
   validates :name,        length: { in: 2..10 }
   validates :description, length: { in: 2..50 }
 
@@ -98,6 +100,7 @@ class AccountBookType
       :charge_account,
       :vat_account,
       :anomaly_account,
+      :instructions,
       :is_default
     ]
   end
