7897f91a6a85bee9966149100afb1e951f19d337
Lola LAI KAM <lailol@directmada.com>
2013-05-09 22:26:44 +0300

    autorisation de visualisation des documents aux collaborateurs, corrigé

--- app/controllers/account/documents_controller.rb
+++ app/controllers/account/documents_controller.rb
@@ -39,7 +39,7 @@ public
   
   def show
     id = BSON::ObjectId.from_string(params[:id])
-    if @user.organization && @user.organization.leader == @user
+    if @user.organization && @user.is_prescriber
       pack_ids = @user.packs.distinct(:_id)
     else
       pack_ids = @user.pack_ids
