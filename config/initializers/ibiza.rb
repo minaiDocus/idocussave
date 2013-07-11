a6f25c2d523240634e3ac990a38a2240942f269a
Lola LAI KAM <lailol@directmada.com>
2013-07-11 10:38:25 +0300

    url différente pour chaque environnement Ibiza

--- config/initializers/ibiza.rb
+++ config/initializers/ibiza.rb
@@ -1,6 +1,6 @@
 module IbizaAPI
   module Config
-    ROOT_URL = 'https://webservice.irf-cloud.com/IRFService/services/IRFService.svc'
+    ROOT_URL = YAML.load_file("#{Rails.root}/config/ibiza.yml")[Rails.env]['root_url']
     COMPANY_PATH = 'Company'
     ACCOUNTS_PATH = 'Accounts'
     PARTNER_ID = '{BE62697A-C2EE-4757-9A86-747A656FF0D7}'
