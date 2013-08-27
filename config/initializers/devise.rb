ea42caf090e11832e9527d590205626fb8c45568
Lola LAI KAM <lailol@directmada.com>
2013-08-27 10:32:21 +0300

    delai d'expiration du lien de réinitialisation du mot de passe passé à 7 jours

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -107,7 +107,7 @@ Devise.setup do |config|
   # Time interval you can reset your password with a reset password key.
   # Don't put a too small interval or your users won't have the time to
   # change their passwords.
-  config.reset_password_within = 2.days
+  config.reset_password_within = 7.days
 
   # ==> Configuration for :token_authenticatable
   # Defines name of the authentication token params key
