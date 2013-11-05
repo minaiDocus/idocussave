9abfe0f279a13a9f8a6d3329d37fde32ed296696
Lola LAI KAM <lailol@directmada.com>
2013-11-05 10:02:02 +0300

    permission accordée aux collaborateurs de visualiser les factures de ses clients

--- app/controllers/account/invoices_controller.rb
+++ app/controllers/account/invoices_controller.rb
@@ -2,7 +2,12 @@ class Account::InvoicesController < Account::AccountController
   def download
     invoice = Invoice.find params[:id]
     filepath = invoice.content.path params[:style]
-    if File.exist?(filepath) && (invoice.user == current_user || (invoice.organization && invoice.organization.leader == current_user) || current_user.is_admin)
+
+    owner = invoice.user
+    current_user.extend_organization_role
+    authorized = false
+    authorized = true if current_user == owner || current_user.is_admin || current_user.my_organization == owner.organization || current_user.customers.include?(owner)
+    if File.exist?(filepath) && authorized
       filename = File.basename filepath
       type = invoice.content_content_type || 'application/pdf'
       send_file(filepath, type: type, filename: filename, x_sendfile: true, disposition: 'inline')
