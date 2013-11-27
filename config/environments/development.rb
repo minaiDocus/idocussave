57c8a589f1f5660362ab4d9d6ec56557d8f92530
Lola LAI KAM <lailol@directmada.com>
2013-11-27 10:13:40 +0300

    changement de l'éditeur de texte par défaut de better_errors à Sublime Text

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,5 @@ Idocus::Application.configure do
   config.assets.debug = false
 
   BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP'] if ENV['TRUSTED_IP']
+  BetterErrors.editor = :sublime
 end
