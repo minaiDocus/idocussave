c00ae7e79037f565b5f61dc4ea5878111de52d23
Lola LAI KAM <lailol@directmada.com>
2014-02-19 13:26:29 +0300

    correction temporaire : certaines images ne sont plus accessible

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,8 @@ Idocus::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  config.serve_static_assets = false
+  # config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
@@ -63,7 +64,8 @@ Idocus::Application.configure do
   config.assets.compile = true
 
   # Generate digests for assets URLs
-  config.assets.digest = true
+  # config.assets.digest = true
+  config.assets.digest = false
 
   # Adding js files
   config.assets.precompile += %w(admin.js
