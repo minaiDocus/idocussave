8b0943e2d7654e63c2a680f2836b3c399854df68
Lola LAI KAM <lailol@directmada.com>
2014-06-06 10:18:31 +0300

    refactorisation de la fonction de mise à jour d'un récupérateur

--- app/services/fiduceo_document_fetcher.rb
+++ app/services/fiduceo_document_fetcher.rb
@@ -1,3 +1,4 @@
+# -*- encoding : UTF-8 -*-
 class FiduceoDocumentFetcher
   class << self
     def initiate_transactions(retrievers=nil)
@@ -64,10 +65,48 @@ class FiduceoDocumentFetcher
         if result['retrievedDocuments']
           transaction.retrieved_document_ids = Array(result['retrievedDocuments']['documentId'])
         end
-        FiduceoTransactionTracker.track(transaction)
         transaction.save
+        update_retriever_by_transaction(transaction)
       else
         nil
+      end
+    end
+
+    def update_retriever_by_transaction(transaction)
+      retriever = transaction.retriever
+      retriever_state_was = retriever.state
+      if transaction.wait_for_user_action? && retriever.processing?
+        retriever.wait_for_user_action
+      elsif transaction.success? && retriever.processing?
+        if retriever.is_selection_needed
+          if retriever.provider?
+            if transaction.retrieved_document_ids.count > 0
+              retriever.wait_selection
+            else
+              retriever.schedule
+            end
+          else
+            retriever.wait_selection
+          end
+          retriever.update_attribute(:is_selection_needed, false)
+        else
+          retriever.schedule
+        end
+        if transaction.retrieved_document_ids.any?
+          retriever.safely.update_attribute(:pending_document_ids, retriever.pending_document_ids + transaction.retrieved_document_ids)
+        end
+      elsif transaction.error? && (retriever.processing? || retriever.wait_for_user_action?)
+        retriever.error
+        if transaction.critical_error?
+          content = ""
+          content << "Utilisateur : #{transaction.user.code}<br>"
+          content << "Récupérateur : #{transaction.retriever.name} - (#{transaction.retriever.service_name})<br>"
+          content << "Transaction : #{transaction.status} - #{transaction.id}<br>"
+          content << "<br><br>#{transaction.events.inspect}"
+          ErrorNotification::EMAILS.each do |email|
+            NotificationMailer.notify(email, "[iDocus] Erreur transaction fiduceo - #{transaction.status}", content).deliver
+          end
+        end
       end
     end
 
