30fb0a84637fddfd879ddd8efe9bb37f32c723a0
Lola LAI KAM <lailol@directmada.com>
2014-09-04 15:48:22 +0300

    ajout d'un niveau de notification pour la livraison des écritures vers Ibiza

--- config/initializers/ibiza.rb
+++ config/initializers/ibiza.rb
@@ -1,10 +1,11 @@
 module IbizaAPI
   module Config
-    CONFIG          = YAML.load_file("#{Rails.root}/config/ibiza.yml")[Rails.env]
-    ROOT_URL        = CONFIG['root_url']
-    COMPANY_PATH    = 'Company'
-    ACCOUNTS_PATH   = 'Accounts'
-    PARTNER_ID      = '{BE62697A-C2EE-4757-9A86-747A656FF0D7}'
-    NOTIFY_ERROR_TO = CONFIG['notify_error_to']
+    CONFIG             = YAML.load_file("#{Rails.root}/config/ibiza.yml")[Rails.env]
+    ROOT_URL           = CONFIG['root_url']
+    COMPANY_PATH       = 'Company'
+    ACCOUNTS_PATH      = 'Accounts'
+    PARTNER_ID         = '{BE62697A-C2EE-4757-9A86-747A656FF0D7}'
+    NOTIFY_ON_DELIVERY = CONFIG['notify_on_delivery']
+    NOTIFY_TO          = CONFIG['notify_to']
   end
 end
