d9475ea2e8e0ad1efe26d765eff159b3892cac3e
Lola LAI KAM <lailol@directmada.com>
2015-01-27 08:35:23 +0300

    affiche aussi les organisations en mode test

--- app/controllers/admin/process_reporting_controller.rb
+++ app/controllers/admin/process_reporting_controller.rb
@@ -4,6 +4,6 @@ class Admin::ProcessReportingController < Admin::AdminController
     year = params[:year].present? ? params[:year].to_i : Time.now.year
     month = params[:month].present? ? params[:month].to_i : Time.now.month
     @time = Time.local(year, month)
-    @organizations = Organization.not_test.asc([:created_at, :name]).entries
+    @organizations = Organization.asc([:created_at, :name]).entries
   end
 end
