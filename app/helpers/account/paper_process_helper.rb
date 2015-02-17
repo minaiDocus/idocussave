2a12fbec2c1aecb579a6d20405919dd7501bb7ef
Lola LAI KAM <lailol@directmada.com>
2015-02-17 11:38:53 +0300

    lien vers le service de suivi de laposte.fr

--- app/helpers/account/paper_process_helper.rb
+++ app/helpers/account/paper_process_helper.rb
@@ -17,4 +17,8 @@ module Account::PaperProcessHelper
       ['Retour', 'return']
     ]
   end
+
+  def link_to_paper_tracking(paper_process)
+    link_to paper_process.tracking_number, "http://www.csuivi.courrier.laposte.fr/suivi/index?id=#{paper_process.tracking_number}"
+  end
 end
