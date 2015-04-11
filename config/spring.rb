c7e9fa535c3631ed01eb01f4364aa61ddbb2e3da
Lola Lai Kam <lola.lai.kam@gmail.com>
2015-04-11 20:04:01 +0300

    lance SimpleCov conditionnellement

--- config/spring.rb
+++ config/spring.rb
@@ -1,4 +1,4 @@
-if ENV['RAILS_ENV'] == 'test'
+if ENV['RAILS_ENV'] == 'test' && ENV['SIMPLECOV'] == 'true'
   require 'simplecov'
   SimpleCov.start 'rails' do
     add_group 'Services', 'app/services'
