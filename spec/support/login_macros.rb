0e5309726acf789ecec40b573cd4985b6aad5793
Lola Lai Kam <lola.lai.kam@gmail.com>
2015-04-12 20:36:35 +0300

    refactorise le macro d'authentification

--- spec/support/login_macros.rb
+++ spec/support/login_macros.rb
@@ -1,17 +1,17 @@
 module LoginMacros
   def login_admin(user=nil, type=:each)
-    before(type) do
-      @user = user || FactoryGirl.create(:admin)
-      page.driver.post user_session_path,
-                       :user => {:email => @user.email, :password => @user.password}
-    end
+    login(:admin, user, type)
   end
 
   def login_user(user=nil, type=:each)
+    login(:user, user, type)
+  end
+
+  def login(user_type, user, type)
     before(type) do
-      @user = user || FactoryGirl.create(:user)
+      @user = user || FactoryGirl.create(user_type)
       page.driver.post user_session_path,
-                       :user => {:email => @user.email, :password => @user.password}
+        user: { email: @user.email, password: @user.password }
     end
   end
 end
