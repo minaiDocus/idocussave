e222ecb5262ae01b9d635a428ae47b48c5ec6067
Lola Lai Kam <lola.lai.kam@gmail.com>
2015-04-14 17:55:59 +0300

    pack n'a plus de "observer"

--- app/services/document_notifier.rb
+++ app/services/document_notifier.rb
@@ -2,20 +2,18 @@
 class DocumentNotifier
   class << self
     def notify_updated
-      Pack.observers.disable :all do
-        updated_packs.each do |owner, packs|
-          if owner.is_document_notifier_active
-            PackMailer.new_document_available(owner, packs).deliver
+      updated_packs.each do |owner, packs|
+        if owner.is_document_notifier_active
+          PackMailer.new_document_available(owner, packs).deliver
+        end
+        collaborators = owner.groups.map(&:collaborators).flatten.uniq
+        collaborators.each do |collaborator|
+          if collaborator.is_document_notifier_active
+            PackMailer.new_document_available(collaborator, packs).deliver
           end
-          collaborators = owner.groups.map(&:collaborators).flatten.uniq
-          collaborators.each do |collaborator|
-            if collaborator.is_document_notifier_active
-              PackMailer.new_document_available(collaborator, packs).deliver
-            end
-          end
-          packs.each do |pack|
-            pack.timeless.update_attribute(:is_update_notified, true)
-          end
+        end
+        packs.each do |pack|
+          pack.timeless.update_attribute(:is_update_notified, true)
         end
       end
     end
