076ebd8400f7c8ccb0350a0141417971c71556d7
Lola Lai Kam <lola.lai.kam@gmail.com>
2015-04-15 20:57:00 +0300

    le patch rspec_rails.rb n'est plus nécessaire

--- lib/patches/rspec_rails.rb
+++
@@ -1,7 +0,0 @@
-# Some gems load ActiveRecord even thought it is not needed, so to fix rspec-rails loading process
-# See rspec-rails-x.x.x/lib/rspec/rails/extensions/active_record/base.rb:13
-module ActiveRecord
-  module VERSION
-    STRING = '3.2.21'
-  end
-end
