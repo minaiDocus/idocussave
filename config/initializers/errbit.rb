a69ff63c57643d6184085faf688e67300c995cd2
Lola Lai Kam <lola.lai.kam@gmail.com>
2015-05-09 16:15:22 +0300

    suppression de "BSON::InvalidObjectId" car n'est plus valide

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -6,7 +6,7 @@ Airbrake.configure do |config|
   config.port    = 443
   config.secure  = config.port == 443
   config.rescue_rake_exceptions = true
-  config.ignore << ["ActionController::UnknownController", "BSON::InvalidObjectId", "Mongoid::Errors::DocumentNotFound"]
+  config.ignore << ["ActionController::UnknownController", "Mongoid::Errors::DocumentNotFound"]
   config.user_attributes = ['id', 'username']
   config.async do |notice|
     Airbrake.sender.delay.send_to_airbrake(notice.to_xml)
