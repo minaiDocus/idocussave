5144aea40038ccce2de71e7ef5b13b2c51a75bc1
Lola Lai Kam <lola.lai.kam@gmail.com>
2015-05-22 19:30:43 +0300

    libellé composé du tiers et du numéro d'origine du document et limité à 30 caractères maximum

--- app/services/preseizure_to_txt_service.rb
+++ app/services/preseizure_to_txt_service.rb
@@ -8,50 +8,33 @@ class PreseizureToTxtService
     data = []
     @preseizures.each do |preseizure|
       preseizure.entries.each do |entry|
-        string = [preseizure.third_party.presence, preseizure.observation.presence].compact.join('. ')
-        labels = string.split('').in_groups_of(30).map(&:join)
-        labels = [' '] unless labels.any?
+        label = [preseizure.third_party.presence, preseizure.piece_number.presence].compact.join(' - ')[0..29]
+        label = ' ' unless label.present?
         line = ' '*256
-        labels.each_with_index do |label, index|
-          if index == 0
-            line[0] = 'M'
-            account_number = entry.account.try(:number) || ''
-            line[1..8]     = account_number[0..7] + (' ' * (8-account_number.size))
-            line[9..10]    = preseizure.report.journal[0..1]
-            line[11..13]   = '000'
-            line[14..19]   = preseizure.date.strftime('%d%m%y') if preseizure.date
-            e = 21 + label[0..19].size - 1
-            line[21..e]    = label[0..19]
-            line[41]       = entry.type == 1 ? 'D' : 'C'
-            line[42]       = entry.amount >= 0.0 ? '+' : '-'
-            line[43..54]   = "%012d" % entry.amount_in_cents
-            line[63..68]   = preseizure.deadline_date.strftime('%d%m%y') if preseizure.deadline_date
-            line[107..109] = 'EUR'
-            line[110..112] = preseizure.report.journal[0..2] if preseizure.report.journal.size > 2
-            if label.size > 20
-              e = 116 + label.size - 1
-              line[116..e] = label
-            end
-            if preseizure.piece
-              file_name = preseizure.piece.number.to_s + '.pdf'
-              e = 181 + file_name.size - 1
-              line[181..e] = file_name
-            end
-            data << line
-          else
-            line2 = line.dup
-            line2[21..40]   = ' '*20
-            e = 21 + label[0..19].size - 1
-            line2[21..e]    = label[0..19]
-            line2[41..54]   = '          0000'
-            line2[116..145] = ' '*30
-            if label.size > 20
-              e = 116 + label.size - 1
-              line2[116..e] = label
-            end
-            data << line2
-          end
+        line[0] = 'M'
+        account_number = entry.account.try(:number) || ''
+        line[1..8]     = account_number[0..7] + (' ' * (8-account_number.size))
+        line[9..10]    = preseizure.report.journal[0..1]
+        line[11..13]   = '000'
+        line[14..19]   = preseizure.date.strftime('%d%m%y') if preseizure.date
+        e = 21 + label[0..19].size - 1
+        line[21..e]    = label[0..19]
+        line[41]       = entry.type == 1 ? 'D' : 'C'
+        line[42]       = entry.amount >= 0.0 ? '+' : '-'
+        line[43..54]   = "%012d" % entry.amount_in_cents
+        line[63..68]   = preseizure.deadline_date.strftime('%d%m%y') if preseizure.deadline_date
+        line[107..109] = 'EUR'
+        line[110..112] = preseizure.report.journal[0..2] if preseizure.report.journal.size > 2
+        if label.size > 20
+          e = 116 + label.size - 1
+          line[116..e] = label
         end
+        if preseizure.piece
+          file_name = preseizure.piece.number.to_s + '.pdf'
+          e = 181 + file_name.size - 1
+          line[181..e] = file_name
+        end
+        data << line
       end
     end
     data.join("\n")
