abb645b76798ea1d67b5ba3d85b3fd4c45a83a0a
Lola Lai Kam <lola.lai.kam@gmail.com>
2015-05-23 21:13:23 +0300

    correction de la gestion des erreurs 404 pour les données dans le dossier public/system/

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,8 +1,8 @@
 # -*- encoding : UTF-8 -*-
 class ErrorsController < ApplicationController
   def routing
-    if params[:a].match(/^system\/contents\//)
-      render nothing: true, status: 404
+    if params[:a].match(/^system.*\/contents\//)
+      raise ActionController::RoutingError.new('Not Found')
     else
       render '/404.html.haml', status: 404, layout: 'error'
     end
