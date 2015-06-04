7896114c2ce57c7f56c95285abffcfb718fc4225
Lola Lai Kam <lola.lai.kam@gmail.com>
2015-06-04 20:39:20 +0300

    fixe la fonction "BSON::ObjectId.legal?", réf : http://sakurity.com/blog/2015/06/04/mongo_ruby_regexp.html

--- config/initializers/BSON/object_id.rb
+++ config/initializers/BSON/object_id.rb
@@ -3,5 +3,10 @@ module BSON
     def as_json(options = {})
       to_s
     end
+
+    # fix vulnerability, DoS and Injection !, see http://sakurity.com/blog/2015/06/04/mongo_ruby_regexp.html
+    def self.legal?(s)
+      /\A\h{24}\z/ === s.to_s
+    end
   end
 end
