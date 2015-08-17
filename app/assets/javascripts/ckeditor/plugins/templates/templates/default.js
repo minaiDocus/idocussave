37adab2571b456b8a95c7186b03ba24ed343c6bc
FlorianBosval <bosval_f@etna-alternance.net>
2015-08-17 17:38:38 +0200

    correction des e-mails de rappel

--- app/assets/javascripts/ckeditor/plugins/templates/templates/default.js
+++ app/assets/javascripts/ckeditor/plugins/templates/templates/default.js
@@ -6,14 +6,14 @@ CKEDITOR.addTemplates("my_templates",
   [
     {
       title: 'Cabinet & iDocus',
-      description: "Rappelez aux clients de nous envoyer leur document.les informations en gras sont à modifier." ,
+      description: "Rappelez aux clients de nous envoyer leur document. Les informations entre double crochés vont être modifiés à l'envoie du mail, tandi que ceux en gras sont à modifier." ,
       html:
         "<div style='font-family:arial'>" +
-        "Bonjour,</br>" +
+        "Bonjour [[nom du client]],</br>" +
         "</br>" +
-        "Vous bénéficiez du service proposé par <b>Nom du cabinet</b> et iDocus pour la dématérialisation de vos pièces comptables.</br>" +
+        "Vous bénéficiez du service proposé par [[nom du cabinet]] et iDocus pour la dématérialisation de vos pièces comptables.</br>" +
         "</br>" +
-        "Nous vous remercions de nous les envoyer au plus vite afin que nous puissions les traiter dés que possible.</br>" +
+        "Nous vous remercions de nous les envoyer au plus vite afin que nous puissions les traiter dès que possible.</br>" +
         "</br>" +
         "Ne tenez pas compte de ce message si vous venez d'effectuer votre envoi.</br>" +
         "</br>" +
@@ -25,56 +25,18 @@ CKEDITOR.addTemplates("my_templates",
     },
     {
       title: 'Cabinet',
-      description: "Rappelez aux clients de nous envoyer leur document.les informations en gras sont à modifier." ,
+      description: "Rappelez aux clients de nous envoyer leur document. Les informations entre double crochés vont être modifiés à l'envoie du mail, tandis que ceux en gras sont à modifier." ,
       html:
         "<div style='font-family:arial'>" +
-        "Bonjour,</br>" +
-        "</br>" +
-        "Vous bénéficiez du service proposé par <b>Nom du cabinet</b> pour la dématérialisation de vos pièces comptables.</br>" +
-        "</br>" +
-        "Nous vous remercions de nous les envoyer au plus vite afin que nous puissions les traiter dés que possible.</br>" +
-        "</br>" +
-        "Ne tenez pas compte de ce message si vous venez d'effectuer votre envoi.</br>" +
-        "</br>" +
-        "Si vous avez des questions sur l'utilisation de ce service, n'hésitez pas à les poser à votre cabinet d’expertise comptable à l’adresse <b>adresse email du cabinet</b> .</br>" +
-        "</br>" +
-        "Cordialement,</br>" +
-        "</br>" +
-        "L'équipe <b>Nom du cabinet</b>.</div>"
-    },
-    {
-      title: 'Cabinet & iDocus: dynamique',
-      description: "Rappelez aux clients de nous envoyer leur document.Les informations entre double crochés vont être modifier à l'envoie du mail." ,
-      html:
-        "<div style='font-family:arial'>" +
-        "Bonjour monsieur/madame [[nom du client]],</br>" +
-        "</br>" +
-        "Vous bénéficiez du service proposé par [[nom du cabinet]] et iDocus pour la dématérialisation de vos pièces comptables.</br>" +
-        "</br>" +
-        "Nous vous remercions de nous les envoyer au plus vite afin que nous puissions les traiter dés que possible.</br>" +
-        "</br>" +
-        "Ne tenez pas compte de ce message si vous venez d'effectuer votre envoi.</br>" +
-        "</br>" +
-        "Si vous avez des questions sur l'utilisation de ce service, n'hésitez pas à les poser à votre cabinet d’expertise comptable à l’adresse [[mail aministrateur du cabinet]] ou à iDocus à l’adresse <a href='mailto:support@idocus.com'>support@idocus.com</a>.</br>" +
-        "</br>" +
-        "Cordialement,</br>" +
-        "</br>" +
-        "L'équipe iDocus.</div>"
-    },
-    {
-      title: 'Cabinet: dynamique',
-      description: "Rappelez aux clients de nous envoyer leur document.Les informations entre double crochés vont être modifier à l'envoie du mail." ,
-      html:
-        "<div style='font-family:arial'>" +
-        "Bonjour monsieur/madame [[nom du client]],</br>" +
+        "Bonjour [[nom du client]],</br>" +
         "</br>" +
         "Vous bénéficiez du service proposé par [[nom du cabinet]] pour la dématérialisation de vos pièces comptables.</br>" +
         "</br>" +
-        "Nous vous remercions de nous les envoyer au plus vite afin que nous puissions les traiter dés que possible.</br>" +
+        "Nous vous remercions de nous les envoyer au plus vite afin que nous puissions les traiter dès que possible.</br>" +
         "</br>" +
         "Ne tenez pas compte de ce message si vous venez d'effectuer votre envoi.</br>" +
         "</br>" +
-        "Si vous avez des questions sur l'utilisation de ce service, n'hésitez pas à les poser à votre cabinet d’expertise comptable à l’adresse [[mail aministrateur du cabinet]] .</br>" +
+        "Si vous avez des questions sur l'utilisation de ce service, n'hésitez pas à les poser à votre cabinet d’expertise comptable à l’adresse <b>adresse email du cabinet</b>.</br>" +
         "</br>" +
         "Cordialement,</br>" +
         "</br>" +
--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -2,9 +2,8 @@
 class ReminderMailer < ActionMailer::Base
   def remind mail, user
     @content = mail.content
-    @content.gsub!(/#{Regexp.quote('[[nom du client]]')}/i, user.last_name)
+    @content.gsub!(/#{Regexp.quote('[[nom du client]]')}/i, user.name)
     @content.gsub!(/#{Regexp.quote('[[nom du cabinet]]')}/i, user.organization.name)
-    @content.gsub!(/#{Regexp.quote('[[mail aministrateur du cabinet]]')}/i, "<a href='mailto:#{user.organization.leader.email}'>#{user.organization.leader.email}</a>")
     mail(to: user.email, subject: mail.subject)
   end
 end
