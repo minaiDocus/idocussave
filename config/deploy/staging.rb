c09a75d23e1f06223473c02d66fcbab14f442c84
Lola Lai Kam <lola.lai.kam@gmail.com>
2015-10-05 15:26:51 +0300

    lance le processus de récupération Dropbox dans l'environnement de préproduction

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -9,3 +9,16 @@ namespace :git do
     %x(git push origin #{branch})
   end
 end
+
+namespace :manager do
+  desc "Start manager"
+  task :start, :roles => :app do
+    run "cd #{release_path}; RAILS_ENV=#{rails_env} bundle exec lib/daemons/manager_ctl start"
+  end
+
+  desc "Stop manager"
+  task :stop, :roles => :app do
+    file_path = "#{current_path}/log/manager.rb.pid"
+    run "if [ -e #{file_path} ]; then kill -TERM $(cat #{file_path}); fi"
+  end
+end
