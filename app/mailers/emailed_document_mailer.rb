c294a71e4f6569e1902bd707829828524a33c212
Lola Lai Kam <lola.lai.kam@gmail.com>
2015-10-06 23:21:39 +0300

    ajoute la référence du mail initial

--- app/mailers/emailed_document_mailer.rb
+++ app/mailers/emailed_document_mailer.rb
@@ -1,24 +1,24 @@
 # -*- encoding : UTF-8 -*-
 class EmailedDocumentMailer < ActionMailer::Base
-  def notify_success(email, emailed_document)
+  def notify_success(email_address, email_message_id, emailed_document)
     @emailed_document = emailed_document
     @journals         = emailed_document.user.account_book_types
     @period_service   = PeriodService.new user: emailed_document.user, current_time: Time.now.beginning_of_month
-    mail(to: email, subject: '[iDocus] Envoi par mail : succès')
+    mail(to: email_address, subject: '[iDocus] Envoi par mail : succès', references: ["<#{email_message_id}>"])
   end
 
-  def notify_failure(email, emailed_document)
+  def notify_failure(email_address, email_message_id, emailed_document)
     @emailed_document = emailed_document
     @journals         = emailed_document.user.account_book_types
     @period_service   = PeriodService.new user: emailed_document.user, current_time: Time.now.beginning_of_month
-    mail(to: email, subject: '[iDocus] Envoi par mail : échec')
+    mail(to: email_address, subject: '[iDocus] Envoi par mail : échec', references: ["<#{email_message_id}>"])
   end
 
-  def notify_error(email, user, attachment_names)
+  def notify_error(email_address, email_message_id, user, attachment_names)
     @user             = user
     @attachment_names = attachment_names
     @journals         = user.account_book_types
     @period_service   = PeriodService.new user: user, current_time: Time.now.beginning_of_month
-    mail(to: email, subject: '[iDocus] Envoi par mail : erreur')
+    mail(to: email_address, subject: '[iDocus] Envoi par mail : erreur', references: ["<#{email_message_id}>"])
   end
 end
