f3f098bf8eb71b7babbd6995e7418c07e0424b89
Lola Lai Kam <lola.lai.kam@gmail.com>
2015-10-13 22:46:57 +0300

    les positions de la charte de nommage des documents doivent être entre 0 et 10

--- app/models/file_naming_policy.rb
+++ app/models/file_naming_policy.rb
@@ -29,6 +29,17 @@ class FileNamingPolicy
   validates_inclusion_of :first_user_identifier, in: %w(code company)
   validates_inclusion_of :second_user_identifier, in: %w(code company), if: -> p { p.second_user_identifier.present? }
 
+  with_options in: 0..10 do |policy|
+    policy.validates_inclusion_of :first_user_identifier_position
+    policy.validates_inclusion_of :second_user_identifier_position
+    policy.validates_inclusion_of :journal_position
+    policy.validates_inclusion_of :period_position
+    policy.validates_inclusion_of :piece_number_position
+    policy.validates_inclusion_of :third_party_position
+    policy.validates_inclusion_of :invoice_number_position
+    policy.validates_inclusion_of :invoice_date_position
+  end
+
   def pre_assignment_needed?
     is_third_party_used || is_invoice_number_used || is_invoice_date_used
   end
