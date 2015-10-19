5e06e59e5e82baba1b3fa2897036e59398a94238
Lola Lai Kam <lola.lai.kam@gmail.com>
2015-10-19 19:04:53 +0300

    suppression d'un paramètre en trop

--- app/services/stop_subscription_service.rb
+++ app/services/stop_subscription_service.rb
@@ -33,7 +33,7 @@ class StopSubscriptionService
     end
     @user.composition.try(:destroy)
     @user.debit_mandate.try(:destroy)
-    DropboxImportFolder.changed(@user, true)
+    DropboxImportFolder.changed(@user)
     @user.valid?
   end
 end
