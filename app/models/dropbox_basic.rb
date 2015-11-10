4008ee64e5cbae3a2b022a5d280ff7764d22f4ad
Lola Lai Kam <lola.lai.kam@gmail.com>
2015-11-10 23:12:50 +0300

    livraison concurrente des fichiers

--- app/models/dropbox_basic.rb
+++ app/models/dropbox_basic.rb
@@ -49,10 +49,10 @@ class DropboxBasic
     end
   end
 
-  def is_up_to_date(remote_filepath, filepath)
+  def is_up_to_date(remote_filepath, filepath, current_client=client)
     path = File.dirname(remote_filepath)
     filename = File.basename(remote_filepath)
-    results = client.search(path, filename, 1)
+    results = current_client.search(path, filename, 1)
     if results.any?
       size = results.first["bytes"]
       if size == File.size(filepath)
@@ -65,37 +65,61 @@ class DropboxBasic
     end
   end
 
-  def is_not_up_to_date(remote_filepath, filepath)
-    !is_up_to_date(remote_filepath, filepath)
+  def is_not_up_to_date(remote_filepath, filepath, current_client=client)
+    !is_up_to_date(remote_filepath, filepath, current_client)
   end
 
   def sync(remote_files)
-    remote_files.each_with_index do |remote_file,index|
-      remote_path = ExternalFileStorage::delivery_path(remote_file, self.path)
-      remote_filepath = File.join(remote_path, remote_file.name)
-      tries = 0
-      begin
-        remote_file.sending!(remote_filepath)
-        print "\t[#{'%0.3d' % (index+1)}] \"#{remote_filepath}\" "
-        if is_not_up_to_date(remote_filepath, remote_file.local_path)
-          print "sending..."
-          client.put_file("#{remote_filepath}", open(remote_file.local_path), true)
-          print "done\n"
-        else
-          print "is up to date\n"
-        end
-        remote_file.synced!
-      rescue => e
-        tries += 1
-        print "failed : [#{e.class}] #{e.message}\n"
-        if tries < 3
-          retry
-        else
-          puts "\t[#{'%0.3d' % (index+1)}] Retrying later"
-          remote_file.not_synced!("[#{e.class}] #{e.message}")
+    queue = Queue.new
+    threads = []
+    semaphore = Mutex.new
+
+    remote_files.each_with_index do |remote_file, index|
+      queue << [remote_file, index]
+    end
+
+    threads_count = 5
+    threads_count = queue.size if queue.size < threads_count
+
+    threads_count.times do
+      threads << Thread.new do
+        current_client = DropboxClient.new(access_token, Dropbox::ACCESS_TYPE)
+        loop do
+          break if queue.empty?
+
+          remote_file, index = queue.pop
+
+          remote_path = ExternalFileStorage::delivery_path(remote_file, path)
+          remote_filepath = File.join(remote_path, remote_file.name)
+          description = "\t[#{'%0.3d' % (index+1)}] \"#{remote_filepath}\""
+          tries = 0
+          begin
+            remote_file.sending!(remote_filepath)
+            if is_not_up_to_date(remote_filepath, remote_file.local_path, current_client)
+              current_client.put_file("#{remote_filepath}", open(remote_file.local_path), true)
+              semaphore.synchronize { puts "#{description} sent" }
+            else
+              semaphore.synchronize { puts "#{description} is up to date" }
+            end
+            remote_file.synced!
+          rescue => e
+            tries += 1
+            semaphore.synchronize { puts "#{description} failed : [#{e.class}] #{e.message}" }
+            if tries < 3
+              retry
+            else
+              semaphore.synchronize { puts "#{description} Retrying later" }
+              remote_file.not_synced!("[#{e.class}] #{e.message}")
+            end
+          end
         end
       end
     end
+
+    threads.each(&:join)
+    remote_files.select do |remote_file|
+      remote_file.state == 'not_synced'
+    end.empty?
   end
 
 private
