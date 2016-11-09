9f10cb211974cfd865c811d327aa360716c2078d
Lola Lai Kam <lola.lai.kam@gmail.com>
2016-11-09 17:14:05 +0300

    ajout de la gestion d'erreur pour la création d'un compte budgea

--- app/services/create_budgea_account.rb
+++ app/services/create_budgea_account.rb
@@ -1,26 +1,58 @@
 # -*- encoding : UTF-8 -*-
 class CreateBudgeaAccount
   def self.execute(user)
-    user.options.with_lock(timeout: 10, retry_sleep: 1) do
-      unless user.budgea_account.try(:access_token).present?
-        client = Budgea::Client.new
-        result = client.create_user
+    new(user).execute
+  end
+
+  def initialize(user)
+    @user = user
+  end
+
+  def execute
+    @user.options.with_lock(timeout: 10, retry_sleep: 1) do
+      unless @user.budgea_account.try(:access_token).present?
+        result = try_request { client.create_user }
         if client.response.code == 200
           budgea_account = BudgeaAccount.new
-          budgea_account.user = user
+          budgea_account.user = @user
           budgea_account.access_token = result
-          client.get_profiles
+          try_request { client.get_profiles }
           if client.response.code == 200
             budgea_account.identifier = client.user_id
           else
-            # TODO handle failure
+            message = "[#{@user.code}] Get identifier<br/>[#{client.response.code}] : #{client.response.body}"
+            notify_failure(message)
           end
           budgea_account.save
         else
-          # TODO handle failure
+          message = "[#{@user.code}] Create user<br/>[#{client.response.code}] : #{client.response.body}"
+          notify_failure(message)
         end
       end
     end
-    user.budgea_account.present?
+    @user.budgea_account.present?
+  end
+
+private
+
+  def client
+    @client ||= Budgea::Client.new
+  end
+
+  def try_request(&block)
+    result = nil
+    3.times do |i|
+      sleep(i) if i > 0
+      result = yield
+      break if client.response.code == 200
+    end
+    result
+  end
+
+  def notify_failure(message)
+    addresses = Array(Settings.notify_errors_to)
+    if addresses.size > 0
+      NotificationMailer.notify(addresses, '[iDocus] Erreur lors de la création de l\'utilisateur Budgea', message).deliver
+    end
   end
 end
