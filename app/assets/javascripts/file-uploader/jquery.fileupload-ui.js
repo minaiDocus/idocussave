f3edede018920715690597fd34a3255c861c7ad2
Thomas Andrieu <t.andrieu@gmail.com>
2016-12-22 04:16:12 +0100

    static assets

--- app/assets/javascripts/file-uploader/jquery.fileupload-ui.js
+++ app/assets/javascripts/file-uploader/jquery.fileupload-ui.js
@@ -36,9 +36,9 @@
             autoUpload: false,
             // The following option limits the number of files that are
             // allowed to be uploaded using this widget:
-            maxNumberOfFiles: undefined,
+            maxNumberOfFiles: 100,
             // The maximum allowed file size:
-            maxFileSize: undefined,
+            maxFileSize: 10000000,
             // The minimum allowed file size:
             minFileSize: 1,
             // The regular expression for allowed file types, matches
