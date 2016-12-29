21d9f7df3d01e17ade08b99593b75aefe522f885
Thomas Andrieu <t.andrieu@gmail.com>
2016-12-29 02:48:37 +0100

    deploy task working

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@ set :application, "idocus"
 
 set :repo_url, "git@github.com:dbarbarossa/idocusave.git"
 
-set :branch, fetch(:branch, "master-v3")
+set :branch, "master-v3"
 
 set :rvm_ruby_version, '2.3.1'
 
@@ -11,12 +11,12 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/dematbox.yml', 'config/fiduceo.yml',
                                                  'config/box.yml', 'config/dematbox_service_api.yml', 'config/knowings.yml', 'config/emailed_document.yml',
                                                  'config/ibiza.yml', 'config/slimpay.yml', 'config/dropbox.yml', 'config/google_drive.yml',
-                                                 'config/pdftk.yml', 'config/smtp.yml')
+                                                 'config/pdftk.yml', 'config/smtp.yml', 'config/ppp_ftp.yml')
 
 namespace :deploy do
   after :updated, :link_production_data do
     on roles(:all) do
-      execute "ln -s /data/idocus/files #{release_path}/files"
+      execute "ln -s /data/idocus/production_data/files #{release_path}/files"
     end
   end
 
