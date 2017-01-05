8ba63be1708a9e6791bca0c33046a153a8d0b40f
Lola Lai Kam <lola.lai.kam@gmail.com>
2017-01-05 16:19:52 +0300

    remplace 'serve_static_assets' par 'serve_static_files'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,8 +34,8 @@ Idocus::Application.configure do
 
   # Disable Rails's static asset server
   # In production, Apache or nginx will already do this
-  # config.serve_static_assets = false
-  config.serve_static_assets = true
+  # config.serve_static_files = false
+  config.serve_static_files = true
 
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,7 +33,7 @@ Idocus::Application.configure do
   config.active_support.deprecation = :stderr
 
   # Configure static asset server for tests with Cache-Control for performance
-  config.serve_static_assets = true
+  config.serve_static_files = true
   config.static_cache_control = 'public, max-age=3600'
 
   # Allow pass debug_assets=true as a query parameter to load pages with unpackaged assets
