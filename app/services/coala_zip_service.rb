b1da45cd677046f612996c851f8285681674698c
Luc Aymar <lucaymar25@gmail.com>
2017-01-07 15:53:14 +0300

    correction export coala sur l export fichier, le dossier temporaire doit etre accessible par le serveur web

--- app/services/coala_zip_service.rb
+++ app/services/coala_zip_service.rb
@@ -7,7 +7,10 @@ class CoalaZipService
 
   def execute
     base_name = @preseizures.first.report.name.gsub(' ', '_')
-    dir = Dir.mktmpdir
+    # initialising a temp dir into rails_app insted of /tmp
+    dir = Dir.mktmpdir(nil, "#{Rails.root}/tmp")
+    FileUtils.chmod(0755, dir)
+
     CoalaZipService.delay_for(6.hours).remove_temp_dir(dir)
     data = PreseizuresToCsv.new(@user, @preseizures, true).execute
     File.open("#{dir}/#{base_name}.csv", 'w') do |f|
@@ -16,7 +19,7 @@ class CoalaZipService
     @preseizures.each do |preseizure|
       entry = preseizure.entries.first
       file_name = preseizure.coala_piece_name + '.pdf'
-      FileUtils.cp preseizure.piece.content.path, File.join(dir, file_name) if preseizure.type.present? && File.exist?(preseizure.piece.try(:content).try(:path).to_s)
+      FileUtils.cp preseizure.piece.content.path, File.join(dir, file_name) if preseizure.type.nil? && File.exist?(preseizure.piece.try(:content).try(:path).to_s)
     end
     file_path = File.join(dir, base_name + '.zip')
     Dir.chdir dir
