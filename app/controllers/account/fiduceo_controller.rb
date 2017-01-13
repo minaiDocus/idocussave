3f325c8e715230bf3baaf6bbcee2c356c600232b
Lola Lai Kam <lola.lai.kam@gmail.com>
2017-01-13 16:11:23 +0300

    désactivation temporaire des pages du service Fiduceo

--- app/controllers/account/fiduceo_controller.rb
+++ app/controllers/account/fiduceo_controller.rb
@@ -3,6 +3,7 @@ class Account::FiduceoController < Account::AccountController
   layout 'layouts/account/retrievers'
 
   before_filter :verify_rights
+  before_filter :redirect_to_root
 
   private
 
@@ -12,5 +13,10 @@ class Account::FiduceoController < Account::AccountController
 
       redirect_to root_path
     end
+  end
+
+  def redirect_to_root
+    flash[:notice] = 'Service temporairement indisponible.'
+    redirect_to root_path
   end
 end
