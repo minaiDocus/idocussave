431449a116b304f9b9ab6a8c7b477941d6ab429e
Lola Lai Kam <lola.lai.kam@gmail.com>
2017-01-18 18:41:30 +0300

    assure qu'une erreur ne bloque pas le verrou

--- app/workers/process_retrieved_data_worker.rb
+++ app/workers/process_retrieved_data_worker.rb
@@ -5,11 +5,18 @@ class ProcessRetrievedDataWorker
   def perform
     $lock = RemoteLock.new(RemoteLock::Adapters::Redis.new(Redis.new))
 
+    error = nil
     begin
       $lock.synchronize('synchronize_retriever_task', expiry: 15.minutes, retries: 1) do
-        ProcessRetrievedData.concurrently(1.minute)
+        begin
+          ProcessRetrievedData.concurrently(1.minute)
+        rescue => e
+          error = e
+        end
       end
     rescue RemoteLock::Error
     end
+    raise error if error
+    true
   end
 end
