d60198859718bc8accad5e2fb08c9522fe713102
Lola Lai Kam <lola.lai.kam@gmail.com>
2017-02-01 20:51:41 +0300

    Initialise les périodes au début de chaque mois

---
+++ app/workers/initialize_periods_worker.rb
@@ -0,0 +1,27 @@
+class InitializePeriodsWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: :default, retry: :false, unique: :until_and_while_executing
+
+  def perform
+    Organization.all.each do |organization|
+      DowngradeSubscription.new(organization.subscription).execute
+      organization.customers.active.each do |customer|
+        begin
+          subscription = customer.subscription
+          if subscription.period_duration == 1 || (subscription.period_duration == 3 && Time.now.month == Time.now.beginning_of_quarter.month) || (subscription.period_duration == 12 && Time.now.month == 1)
+            DowngradeSubscription.new(subscription).execute
+          end
+          subscription.current_period
+          if subscription.period_duration != 1
+            time = 1.month.ago
+            period = subscription.find_period time
+            PeriodBillingService.new(period).save(time.month) if period
+          end
+          print '.'
+        rescue
+          puts "Can't generate period for user #{customer.info}, probably lack of subscription entry."
+        end
+      end
+    end
+  end
+end
