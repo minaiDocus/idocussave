9b543662e5dfb45cade0ed692eb55eb532bde350
Lola Lai Kam <lola.lai.kam@gmail.com>
2017-02-17 23:32:43 +0300

    modification de l'affichage des opérations bancaires récupérées

--- app/controllers/account/organization/retrieved_banking_operations_controller.rb
+++ app/controllers/account/organization/retrieved_banking_operations_controller.rb
@@ -2,10 +2,12 @@
 class Account::Organization::RetrievedBankingOperationsController < Account::Organization::RetrieverController
   def index
     bank_account_ids = @customer.bank_accounts.used.map(&:id)
-    operations = @customer.operations.retrieved.where(bank_account_id: bank_account_ids)
-    @operations = Operation.search_for_collection(operations, search_terms(params[:banking_operation_contains])).order(sort_column => sort_direction).includes(:bank_account)
-    @operations_count = @operations.count
-    @operations = @operations.page(params[:page]).per(params[:per_page])
+    operations = @customer.operations.retrieved.where(
+      Operation.arel_table[:bank_account_id].in(bank_account_ids).or(
+        Operation.arel_table[:processed_at].not_eq(nil)
+      )
+    )
+    @operations = Operation.search_for_collection(operations, search_terms(params[:banking_operation_contains])).order(sort_column => sort_direction).includes(:bank_account).page(params[:page]).per(params[:per_page])
   end
 
 private
--- app/controllers/account/retrieved_banking_operations_controller.rb
+++ app/controllers/account/retrieved_banking_operations_controller.rb
@@ -2,10 +2,12 @@
 class Account::RetrievedBankingOperationsController < Account::RetrieverController
   def index
     bank_account_ids = @user.bank_accounts.used.map(&:id)
-    operations = @user.operations.retrieved.where(bank_account_id: bank_account_ids)
-    @operations = Operation.search_for_collection(operations, search_terms(params[:banking_operation_contains])).order(sort_column => sort_direction).includes(:bank_account)
-    @operations_count = @operations.count
-    @operations = @operations.page(params[:page]).per(params[:per_page])
+    operations = @user.operations.retrieved.where(
+      Operation.arel_table[:bank_account_id].in(bank_account_ids).or(
+        Operation.arel_table[:processed_at].not_eq(nil)
+      )
+    )
+    @operations = Operation.search_for_collection(operations, search_terms(params[:banking_operation_contains])).order(sort_column => sort_direction).includes(:bank_account).page(params[:page]).per(params[:per_page])
     @is_filter_empty = search_terms(params[:banking_operation_contains]).blank?
   end
 
