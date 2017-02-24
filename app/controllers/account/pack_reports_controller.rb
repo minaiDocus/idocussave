ea065f1e97d28af45db23a8b5608ba230946cd55
Luc Aymar <lucaymar25@gmail.com>
2017-02-24 18:49:29 +0300

    desactive temporairement la livraison vers ibiza et traitement pre_affectation pour la mise à jour ibiza

--- app/controllers/account/pack_reports_controller.rb
+++ app/controllers/account/pack_reports_controller.rb
@@ -23,7 +23,7 @@ class Account::PackReportsController < Account::OrganizationController
   def deliver
     preseizures = @report.preseizures.by_position.not_locked.not_delivered
 
-    CreatePreAssignmentDeliveryService.new(preseizures).execute
+    #CreatePreAssignmentDeliveryService.new(preseizures).execute
 
     respond_to do |format|
       format.json { render json: { status: :ok } }
