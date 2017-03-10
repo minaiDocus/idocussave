3bff1f49a011c8d7baf0859d7afa9d4a24478b82
Lola Lai Kam <lola.lai.kam@gmail.com>
2017-03-10 18:51:20 +0300

    génére une entrée PeriodBilling pour le mois précédent pour les comptes clôturés

--- app/workers/initialize_periods_worker.rb
+++ app/workers/initialize_periods_worker.rb
@@ -5,13 +5,15 @@ class InitializePeriodsWorker
   def perform
     Organization.all.each do |organization|
       DowngradeSubscription.new(organization.subscription).execute
-      organization.customers.active.each do |customer|
+      organization.customers.active_at(1.month.ago).each do |customer|
         begin
           subscription = customer.subscription
-          if subscription.period_duration == 1 || (subscription.period_duration == 3 && Time.now.month == Time.now.beginning_of_quarter.month) || (subscription.period_duration == 12 && Time.now.month == 1)
-            DowngradeSubscription.new(subscription).execute
+          if customer.active?
+            if subscription.period_duration == 1 || (subscription.period_duration == 3 && Time.now.month == Time.now.beginning_of_quarter.month) || (subscription.period_duration == 12 && Time.now.month == 1)
+              DowngradeSubscription.new(subscription).execute
+            end
+            subscription.current_period
           end
-          subscription.current_period
           if subscription.period_duration != 1
             time = 1.month.ago
             period = subscription.find_period time
