0f8013b2db756b79febcb7a99f42c61b7db674bc
Lola Lai Kam <lola.lai.kam@gmail.com>
2017-04-06 17:16:56 +0300

    serialise le temps avant envoi en traitement en tâche de fond

--- app/services/document_notifier.rb
+++ app/services/document_notifier.rb
@@ -3,14 +3,14 @@ class DocumentNotifier
   def self.notify_updated(start_at, end_at)
     updated_packs(start_at, end_at).each do |owner, packs|
       if owner.is_document_notifier_active
-        PackMailer.new_document_available(owner, packs, start_at, end_at).deliver_later
+        PackMailer.new_document_available(owner, packs, start_at.to_i, end_at.to_i).deliver_later
       end
 
       collaborators = owner.groups.map(&:collaborators).flatten.uniq
 
       collaborators.each do |collaborator|
         if collaborator.is_document_notifier_active
-          PackMailer.new_document_available(collaborator, packs, start_at, end_at).deliver_later
+          PackMailer.new_document_available(collaborator, packs, start_at.to_i, end_at.to_i).deliver_later
         end
       end
 
