7ce6698bad954ad5d27c470666e0cd3d1973c2c9
Luc Aymar <lucaymar25@gmail.com>
2017-05-12 13:23:04 +0300

    correction affichage date documents en attente ocr

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -17,7 +17,7 @@ class Admin::AdminController < ApplicationController
   def ocr_needed_temp_packs
     @ocr_needed_temp_packs = TempDocument.where(state: 'ocr_needed').group(:temp_pack_id).includes(:temp_pack).map do |data|
       object = OpenStruct.new
-      object.date           = data.temp_pack.temp_documents.try(:updated_at)
+      object.date           = data.try(:updated_at)
       object.name           = data.temp_pack.name.sub(/ all\z/, '')
       object.document_count = data.temp_pack.temp_documents.ocr_needed.count
       object.message        = false
