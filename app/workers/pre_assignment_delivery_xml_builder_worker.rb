07a11fd1da2ac9c3d495729ec52d0adf01a3b5d2
Lola Lai Kam <lola.lai.kam@gmail.com>
2017-07-25 17:52:48 +0300

    augmente le nombre de tentative maximum pour obtenir le verroui

--- app/workers/pre_assignment_delivery_xml_builder_worker.rb
+++ app/workers/pre_assignment_delivery_xml_builder_worker.rb
@@ -4,7 +4,7 @@ class PreAssignmentDeliveryXmlBuilderWorker
 
   def perform(delivery_id)
     delivery = PreAssignmentDelivery.find(delivery_id)
-    $remote_lock.synchronize "PreAssignmentDeliveryXmlBuilder-#{delivery.organization.ibiza.id}", expiry: 1.hour, retries: 10 do
+    $remote_lock.synchronize "PreAssignmentDeliveryXmlBuilder-#{delivery.organization.ibiza.id}", expiry: 1.hour, retries: 100 do
       PreAssignmentDeliveryXmlBuilder.new(delivery_id).execute
     end
   end
