d3e2eb89ef8e4ef699125e55a7585aca0daf80af
Lola Lai Kam <lola.lai.kam@gmail.com>
2017-08-16 20:28:45 +0300

    tests obsolètes de Fiduceo commentés

--- spec/services/fetch_fiduceo_data_spec.rb
+++ spec/services/fetch_fiduceo_data_spec.rb
@@ -1,104 +1,105 @@
 # -*- encoding : UTF-8 -*-
 require 'spec_helper'
 
+# TODO : check if it is still needed
 describe FetchFiduceoData do
-  context 'document' do
-    before(:all) do
-      @user = FactoryGirl.create :user, code: 'IDO%0001'
-      @user.options = UserOptions.create(user_id: @user.id)
-      @journal = FactoryGirl.create :account_book_type, user_id: @user.id
+  # context 'document' do
+  #   before(:all) do
+  #     @user = FactoryGirl.create :user, code: 'IDO%0001'
+  #     @user.options = UserOptions.create(user_id: @user.id)
+  #     @journal = FactoryGirl.create :account_book_type, user_id: @user.id
 
-      @connector = Connector.new
-      @connector.name            = 'OVH'
-      @connector.capabilities    = ['document']
-      @connector.apis            = ['fiduceo']
-      @connector.active_apis     = ['fiduceo']
-      @connector.fiduceo_ref     = '4d98668c287db3fb58b3f6e9'
-      @connector.combined_fields = {
-        login: {
-          label:        'Identifiant',
-          type:         'text',
-          regex:        nil,
-          fiduceo_name: 'login'
-        },
-        password: {
-          label:        'Mot de passe',
-          type:         'password',
-          regex:        nil,
-          fiduceo_name: 'pass'
-        }
-      }
-      @connector.save
+  #     @connector = Connector.new
+  #     @connector.name            = 'OVH'
+  #     @connector.capabilities    = ['document']
+  #     @connector.apis            = ['fiduceo']
+  #     @connector.active_apis     = ['fiduceo']
+  #     @connector.fiduceo_ref     = '4d98668c287db3fb58b3f6e9'
+  #     @connector.combined_fields = {
+  #       login: {
+  #         label:        'Identifiant',
+  #         type:         'text',
+  #         regex:        nil,
+  #         fiduceo_name: 'login'
+  #       },
+  #       password: {
+  #         label:        'Mot de passe',
+  #         type:         'password',
+  #         regex:        nil,
+  #         fiduceo_name: 'pass'
+  #       }
+  #     }
+  #     @connector.save
 
-      @retriever = Retriever.new
-      @retriever.user      = @user
-      @retriever.connector = @connector
-      @retriever.journal   = @journal
-      @retriever.name      = 'OVH'
-      @retriever.param1 = {
-        name: 'login',
-        type: 'text',
-        value: 'John Doe'
-      }
-      @retriever.param2 = {
-        name: 'password',
-        type: 'password',
-        value: '12345'
-      }
-      @retriever.save
-      @retriever.reload
+  #     @retriever = Retriever.new
+  #     @retriever.user      = @user
+  #     @retriever.connector = @connector
+  #     @retriever.journal   = @journal
+  #     @retriever.name      = 'OVH'
+  #     @retriever.param1 = {
+  #       name: 'login',
+  #       type: 'text',
+  #       value: 'John Doe'
+  #     }
+  #     @retriever.param2 = {
+  #       name: 'password',
+  #       type: 'password',
+  #       value: '12345'
+  #     }
+  #     @retriever.save
+  #     @retriever.reload
 
-      VCR.use_cassette('fiduceo/create_user') do
-        FiduceoUser.new(@user).create
-      end
+  #     VCR.use_cassette('fiduceo/create_user') do
+  #       FiduceoUser.new(@user).create
+  #     end
 
-      VCR.use_cassette('fiduceo/create_fiduceo_connection') do
-        SyncFiduceoConnection.execute(@retriever, false)
-      end
-    end
+  #     VCR.use_cassette('fiduceo/create_fiduceo_connection') do
+  #       SyncFiduceoConnection.execute(@retriever, false)
+  #     end
+  #   end
 
-    after(:each) do
-      TempDocument.destroy_all
-    end
+  #   after(:each) do
+  #     TempDocument.destroy_all
+  #   end
 
-    context 'no preexisting document' do
-      it 'creates a document' do
-        expect(@user.temp_documents.count).to eq 0
+  #   context 'no preexisting document' do
+  #     it 'creates a document' do
+  #       expect(@user.temp_documents.count).to eq 0
 
-        VCR.use_cassette('fiduceo/fetch_documents') do
-          FetchFiduceoData.execute(@retriever)
-        end
+  #       VCR.use_cassette('fiduceo/fetch_documents') do
+  #         FetchFiduceoData.execute(@retriever)
+  #       end
 
-        expect(@user.temp_documents.count).to eq 1
-      end
-    end
+  #       expect(@user.temp_documents.count).to eq 1
+  #     end
+  #   end
 
-    context 'a document already exist' do
-      it 'does not create a document' do
-        pack = TempPack.find_or_create_by_name "#{@user.code} #{@journal.name} #{Time.now.strftime('%Y%M')}"
-        options = {
-          user_id:               @user.id,
-          delivery_type:         'retriever',
-          api_id:                '585a9f59498ea472a02d7dcc',
-          api_name:              'fiduceo',
-          metadata:              { date: Date.parse('18-09-2014') },
-          is_content_file_valid: true
-        }
-        file = File.open(Rails.root.join('spec', 'support', 'files', '2pages.pdf'), 'r')
-        temp_document = pack.add file, options
-        @retriever.temp_documents << temp_document
-        file.close
+  #   context 'a document already exist' do
+  #     it 'does not create a document' do
+  #       pack = TempPack.find_or_create_by_name "#{@user.code} #{@journal.name} #{Time.now.strftime('%Y%M')}"
+  #       options = {
+  #         user_id:               @user.id,
+  #         delivery_type:         'retriever',
+  #         api_id:                '585a9f59498ea472a02d7dcc',
+  #         api_name:              'fiduceo',
+  #         metadata:              { date: Date.parse('18-09-2014') },
+  #         is_content_file_valid: true
+  #       }
+  #       file = File.open(Rails.root.join('spec', 'support', 'files', '2pages.pdf'), 'r')
+  #       temp_document = pack.add file, options
+  #       @retriever.temp_documents << temp_document
+  #       file.close
 
-        expect(@user.temp_documents.count).to eq 1
+  #       expect(@user.temp_documents.count).to eq 1
 
-        VCR.use_cassette('fiduceo/fetch_documents') do
-          FetchFiduceoData.execute(@retriever)
-        end
+  #       VCR.use_cassette('fiduceo/fetch_documents') do
+  #         FetchFiduceoData.execute(@retriever)
+  #       end
 
-        expect(@user.temp_documents.count).to eq 1
-      end
-    end
-  end
+  #       expect(@user.temp_documents.count).to eq 1
+  #     end
+  #   end
+  # end
 
   # TODO add test for bank account and operations
 end
--- spec/services/sync_fiduceo_connection_spec.rb
+++ spec/services/sync_fiduceo_connection_spec.rb
@@ -1,108 +1,109 @@
 # -*- encoding : UTF-8 -*-
 require 'spec_helper'
 
+# TODO : check if it is still needed
 describe SyncFiduceoConnection do
-  before(:all) do
-    @user = FactoryGirl.create :user, code: 'IDO%0001'
-    @user.options = UserOptions.create(user_id: @user.id)
-    @journal = FactoryGirl.create :account_book_type, user_id: @user.id
+  # before(:all) do
+  #   @user = FactoryGirl.create :user, code: 'IDO%0001'
+  #   @user.options = UserOptions.create(user_id: @user.id)
+  #   @journal = FactoryGirl.create :account_book_type, user_id: @user.id
 
-    @connector = Connector.new
-    @connector.name            = 'OVH'
-    @connector.capabilities    = ['document']
-    @connector.apis            = ['fiduceo']
-    @connector.active_apis     = ['fiduceo']
-    @connector.fiduceo_ref     = '4d98668c287db3fb58b3f6e9'
-    @connector.combined_fields = {
-      login: {
-        label:        'Identifiant',
-        type:         'text',
-        regex:        nil,
-        fiduceo_name: 'login'
-      },
-      password: {
-        label:        'Mot de passe',
-        type:         'password',
-        regex:        nil,
-        fiduceo_name: 'pass'
-      }
-    }
-    @connector.save
+  #   @connector = Connector.new
+  #   @connector.name            = 'OVH'
+  #   @connector.capabilities    = ['document']
+  #   @connector.apis            = ['fiduceo']
+  #   @connector.active_apis     = ['fiduceo']
+  #   @connector.fiduceo_ref     = '4d98668c287db3fb58b3f6e9'
+  #   @connector.combined_fields = {
+  #     login: {
+  #       label:        'Identifiant',
+  #       type:         'text',
+  #       regex:        nil,
+  #       fiduceo_name: 'login'
+  #     },
+  #     password: {
+  #       label:        'Mot de passe',
+  #       type:         'password',
+  #       regex:        nil,
+  #       fiduceo_name: 'pass'
+  #     }
+  #   }
+  #   @connector.save
 
-    @retriever = Retriever.new
-    @retriever.user      = @user
-    @retriever.connector = @connector
-    @retriever.journal   = @journal
-    @retriever.name      = 'OVH'
-    @retriever.param1 = {
-      name: 'login',
-      type: 'text',
-      value: 'John Doe'
-    }
-    @retriever.param2 = {
-      name: 'password',
-      type: 'password',
-      value: '12345'
-    }
-    @retriever.save
-    @retriever.reload
+  #   @retriever = Retriever.new
+  #   @retriever.user      = @user
+  #   @retriever.connector = @connector
+  #   @retriever.journal   = @journal
+  #   @retriever.name      = 'OVH'
+  #   @retriever.param1 = {
+  #     name: 'login',
+  #     type: 'text',
+  #     value: 'John Doe'
+  #   }
+  #   @retriever.param2 = {
+  #     name: 'password',
+  #     type: 'password',
+  #     value: '12345'
+  #   }
+  #   @retriever.save
+  #   @retriever.reload
 
-    VCR.use_cassette('fiduceo/create_user') do
-      FiduceoUser.new(@user).create
-    end
-  end
+  #   VCR.use_cassette('fiduceo/create_user') do
+  #     FiduceoUser.new(@user).create
+  #   end
+  # end
 
-  it 'creates a connection successfully' do
-    expect(@retriever.state).to eq 'configuring'
-    VCR.use_cassette('fiduceo/create_fiduceo_connection') do
-      SyncFiduceoConnection.execute(@retriever, false)
-    end
-    expect(@retriever.param1).to be_present
-    expect(@retriever.param2).to be_nil
-    expect(@retriever.fiduceo_id).to be_present
-    expect(@retriever.state).to eq 'ready'
-    expect(@retriever.fiduceo_state).to eq 'successful'
-  end
+  # it 'creates a connection successfully' do
+  #   expect(@retriever.state).to eq 'configuring'
+  #   VCR.use_cassette('fiduceo/create_fiduceo_connection') do
+  #     SyncFiduceoConnection.execute(@retriever, false)
+  #   end
+  #   expect(@retriever.param1).to be_present
+  #   expect(@retriever.param2).to be_nil
+  #   expect(@retriever.fiduceo_id).to be_present
+  #   expect(@retriever.state).to eq 'ready'
+  #   expect(@retriever.fiduceo_state).to eq 'successful'
+  # end
 
-  it 'updates a connection successfully' do
-    @retriever.param1 = {
-      name: 'login',
-      type: 'text',
-      value: 'John Doe 2'
-    }
-    @retriever.save
-    @retriever.reload
-    @retriever.configure_connection
+  # it 'updates a connection successfully' do
+  #   @retriever.param1 = {
+  #     name: 'login',
+  #     type: 'text',
+  #     value: 'John Doe 2'
+  #   }
+  #   @retriever.save
+  #   @retriever.reload
+  #   @retriever.configure_connection
 
-    expect(@retriever.state).to eq 'configuring'
-    VCR.use_cassette('fiduceo/update_fiduceo_connection') do
-      SyncFiduceoConnection.execute(@retriever, false)
-    end
-    expect(@retriever.param1).to be_present
-    expect(@retriever.param2).to be_nil
-    expect(@retriever.state).to eq 'ready'
-    expect(@retriever.fiduceo_state).to eq 'successful'
-  end
+  #   expect(@retriever.state).to eq 'configuring'
+  #   VCR.use_cassette('fiduceo/update_fiduceo_connection') do
+  #     SyncFiduceoConnection.execute(@retriever, false)
+  #   end
+  #   expect(@retriever.param1).to be_present
+  #   expect(@retriever.param2).to be_nil
+  #   expect(@retriever.state).to eq 'ready'
+  #   expect(@retriever.fiduceo_state).to eq 'successful'
+  # end
 
-  it 'forces a synchronization successfully' do
-    @retriever.run
+  # it 'forces a synchronization successfully' do
+  #   @retriever.run
 
-    expect(@retriever.state).to eq 'running'
-    VCR.use_cassette('fiduceo/synchronize_fiduceo_connection') do
-      SyncFiduceoConnection.execute(@retriever, false)
-    end
-    expect(@retriever.param1).to be_present
-    expect(@retriever.param2).to be_nil
-    expect(@retriever.state).to eq 'ready'
-    expect(@retriever.fiduceo_state).to eq 'successful'
-  end
+  #   expect(@retriever.state).to eq 'running'
+  #   VCR.use_cassette('fiduceo/synchronize_fiduceo_connection') do
+  #     SyncFiduceoConnection.execute(@retriever, false)
+  #   end
+  #   expect(@retriever.param1).to be_present
+  #   expect(@retriever.param2).to be_nil
+  #   expect(@retriever.state).to eq 'ready'
+  #   expect(@retriever.fiduceo_state).to eq 'successful'
+  # end
 
-  it 'destroys a connection successfully' do
-    @retriever.destroy_connection
+  # it 'destroys a connection successfully' do
+  #   @retriever.destroy_connection
 
-    VCR.use_cassette('fiduceo/destroy_fiduceo_connection') do
-      SyncFiduceoConnection.execute(@retriever)
-    end
-    expect(@retriever).to be_destroyed
-  end
+  #   VCR.use_cassette('fiduceo/destroy_fiduceo_connection') do
+  #     SyncFiduceoConnection.execute(@retriever)
+  #   end
+  #   expect(@retriever).to be_destroyed
+  # end
 end
