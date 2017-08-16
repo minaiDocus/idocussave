f75af7f95196eb46bb4171cc2f0a7d0c905cf48d
Lola Lai Kam <lola.lai.kam@gmail.com>
2017-08-16 19:20:19 +0300

    refactorisation de "UploadedDocumentPresenter"

--- app/presenters/uploaded_document_presenter.rb
+++ app/presenters/uploaded_document_presenter.rb
@@ -3,18 +3,17 @@ class UploadedDocumentPresenter
     @uploaded_document = uploaded_document
   end
 
-
   def to_json
-    file   = {}
+    file = {}
     data = []
-    
+
     if @uploaded_document.valid?
       temp_document = @uploaded_document.temp_document
 
-      file[:name]        = temp_document.original_file_name
       file[:created_at] = I18n.l(temp_document.created_at)
-      file[:new_name] = temp_document.content_file_name
-      
+      file[:name]       = temp_document.original_file_name
+      file[:new_name]   = temp_document.content_file_name
+
       if temp_document.state == 'bundle_needed'
         file[:message] = 'vos documents sont en-cours de traitement, ils seront visibles dans quelques heures dans votre espace'
       end
