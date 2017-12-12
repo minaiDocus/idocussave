6391c410a194f8a4f0d8b24fb8b47acfc723043d
Lola Lai Kam <lola.lai.kam@gmail.com>
2017-12-12 21:55:26 +0300

    rend explicite qu'il ne faut pas répondre au mail de rappel personnalisé par l'organisation

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -1,10 +1,12 @@
 # -*- encoding : UTF-8 -*-
 class ReminderMailer < ActionMailer::Base
+  default reply_to: nil
+
   def remind(mail, user)
     @content = mail.content
     @content.gsub!(/#{Regexp.quote('[[nom du client]]')}/i, user.name)
     @content.gsub!(/#{Regexp.quote('[[nom du cabinet]]')}/i, user.organization.name)
-    
+
     mail(to: user.email, subject: mail.subject)
   end
 end
