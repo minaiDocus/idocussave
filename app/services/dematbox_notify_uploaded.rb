cbbb337747fe48ead1129b4d9c6234425efd7c3f
Lola Lai Kam <lola.lai.kam@gmail.com>
2018-01-30 23:11:12 +0300

    refactorisation et gestion de l'erreur où le document a déjà été notifié

--- app/services/dematbox_notify_uploaded.rb
+++ app/services/dematbox_notify_uploaded.rb
@@ -2,19 +2,27 @@ class DematboxNotifyUploaded
   def self.execute(temp_document_id)
     temp_document = TempDocument.find(temp_document_id)
 
-    pages_number = DocumentTools.pages_number(temp_document.content.path)
+    if temp_document.dematbox_box_id && temp_document.dematbox_doc_id
+      pages_number = DocumentTools.pages_number(temp_document.content.path)
+      message = 'Envoi OK : %02d p.' % pages_number
 
-    message = 'Envoi OK : %02d p.' % pages_number
+      begin
+        result = DematboxApi.notify_uploaded temp_document.dematbox_doc_id, temp_document.dematbox_box_id, message
+      rescue Savon::SOAPFault => e
+        if e.match(/702:DocId already notified/)
+          result = true
+        else
+          raise
+        end
+      end
 
-    result = DematboxApi.notify_uploaded temp_document.dematbox_doc_id, temp_document.dematbox_box_id, message if temp_document.dematbox_box_id && temp_document.dematbox_doc_id
+      if result == '200:OK' || result == true
+        temp_document.update(dematbox_is_notified: true, dematbox_notified_at: Time.now)
 
-    if result == '200:OK'
-      temp_document.dematbox_is_notified = true
-      temp_document.dematbox_notified_at = Time.now
-      result = temp_document.save
-
-      if temp_document.uploaded?
-        DematboxServiceApi.upload_notification temp_document.dematbox_doc_id, temp_document.dematbox_box_id if temp_document.dematbox_box_id && temp_document.dematbox_doc_id
+        # Note : not used, since we do not process OCR through Sagemcom anymore
+        # if temp_document.uploaded?
+        #   DematboxServiceApi.upload_notification temp_document.dematbox_doc_id, temp_document.dematbox_box_id
+        # end
       end
     end
   end
