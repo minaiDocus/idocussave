2e329c8ab6fb49b894bb060329652399cc0037e8
Lola Lai Kam <lola.lai.kam@gmail.com>
2018-02-21 21:36:08 +0300

    supprime les espaces inutiles

--- app/mailers/emailed_document_mailer.rb
+++ app/mailers/emailed_document_mailer.rb
@@ -1,36 +1,26 @@
-# -*- encoding : UTF-8 -*-
 class EmailedDocumentMailer < ActionMailer::Base
   def notify_success(email, emailed_document)
     @emailed_document = emailed_document
-
     @journals         = emailed_document.user.account_book_types
-
     @period_service   = PeriodService.new user: emailed_document.user, current_time: Time.now.beginning_of_month
 
     mail(to: email.from, subject: "[iDocus] Envoi par mail (#{email.subject}) : succès", references: ["<#{email.message_id}>"])
   end
 
-
   def notify_failure(email, emailed_document)
     @emailed_document = emailed_document
-
     @journals         = emailed_document.user.account_book_types
-
     @period_service   = PeriodService.new user: emailed_document.user, current_time: Time.now.beginning_of_month
 
     mail(to: email.from, subject: "[iDocus] Envoi par mail (#{email.subject}) : échec", references: ["<#{email.message_id}>"])
   end
 
-
   def notify_error(email, attachment_names)
     @user             = email.to_user
-
     @attachment_names = attachment_names
-
     @journals         = @user.account_book_types
-
     @period_service   = PeriodService.new user: @user, current_time: Time.now.beginning_of_month
-    
+
     mail(to: email.from, subject: "[iDocus] Envoi par mail (#{email.subject}) : erreur", references: ["<#{email.message_id}>"])
   end
 end
