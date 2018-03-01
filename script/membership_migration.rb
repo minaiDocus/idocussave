1cffa0783a5fc4e6db175342fbd969bb6a6c6032
Lola Lai Kam <lola.lai.kam@gmail.com>
2018-03-01 23:33:05 +0300

    mise à jour du script de migration

--- script/membership_migration.rb
+++ script/membership_migration.rb
@@ -1,36 +1,41 @@
 ### Migrate data for membership system
 
-# Create a membership for each collaborator
-# TODO : create membership to all 3 organizations for Extentis
-# TODO : remove code from collaborator
-# TODO : empty organization_id for collaborator
-User.prescribers.each do |u|
-  m = Member.find_by user_id: u.id, organization_id: u.organization_id
-  next if m
+puts 'Create a membership for each collaborator'
+User.transaction do
+  User.prescribers.each do |u|
+    m = Member.find_by user_id: u.id, organization_id: u.organization_id
+    next if m
 
-  if u.organization.leader_id == u.id
-    role = Member::ADMIN
-  else
-    role = Member::COLLABORATOR
+    if u.organization.leader_id == u.id || u.is_admin
+      role = Member::ADMIN
+    else
+      role = Member::COLLABORATOR
+    end
+
+    member = Member.create(
+      user_id: u.id,
+      organization_id: u.organization_id,
+      role: role,
+      code: u.code,
+      created_at: u.created_at,
+      manage_groups: u.organization_rights_is_groups_management_authorized,
+      manage_collaborators: u.organization_rights_is_collaborators_management_authorized,
+      manage_customers: u.organization_rights_is_customers_management_authorized,
+      manage_journals: u.organization_rights_is_journals_management_authorized,
+      manage_customer_journals: u.organization_rights_is_customer_journals_management_authorized
+    )
+
+    # empties organization_id for collaborator
+    # removes code from user
+    # user.update(code: nil, organization_id: nil) if member.persisted?
+
+    print '.'
   end
+end
 
-  Member.create(
-    user_id: u.id,
-    organization_id: u.organization_id,
-    role: role,
-    code: u.code,
-    created_at: u.created_at,
-    manage_groups: u.organization_rights_is_groups_management_authorized,
-    manage_collaborators: u.organization_rights_is_collaborators_management_authorized,
-    manage_customers: u.organization_rights_is_customers_management_authorized,
-    manage_journals: u.organization_rights_is_journals_management_authorized,
-    manage_customer_journals: u.organization_rights_is_customer_journals_management_authorized
-  )
+# TODO : create membership to all 3 organizations for Extentis
 
-  print '.'
-end; nil
-
-# Reassign group membership from collaborator to member
+puts 'Reassign group membership from collaborator to member'
 Group.all.each do |g|
   g.users.prescribers.each do |u|
     m = Member.find_by user_id: u.id, organization_id: g.organization_id
@@ -39,9 +44,9 @@ Group.all.each do |g|
     m.save
     print '.'
   end
-end; nil
+end
 
-# Migrate parent/child relationship
+puts 'Migrate parent/child relationship'
 User.customers.active.where(manager_id: nil).each do |u|
   c = User.find_by id: u.parent_id
   next unless c
@@ -49,4 +54,6 @@ User.customers.active.where(manager_id: nil).each do |u|
   next unless m
   u.update(manager: m)
   print '.'
-end; nil
+end
+
+puts 'Done'
