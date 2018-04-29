fa4e8c22ced7e90b5e19102fb36472aba1032adf
Luc Aymar <lucaymar25@gmail.com>
2018-04-29 14:27:34 +0300

    Ajout d un nouveau palier de remise special pour une organisation

--- app/services/discount_billing_service.rb
+++ app/services/discount_billing_service.rb
@@ -23,12 +23,12 @@ class DiscountBillingService
   end
 
   def title
-    if unit_amount[:subscription] < 0
-      if unit_amount[:retriever] < 0
-        "Remise sur CA (Abo. mensuels : #{unit_amount[:subscription]} € x #{subscription_quota}, iDofacb. : #{unit_amount[:retriever]} € x #{quantity_of(:retriever)})"
-      else
-        "Remise sur CA (Abo. mensuels : #{unit_amount[:subscription]} € x #{subscription_quota})"
-      end
+    if unit_amount(:subscription) < 0 && unit_amount(:retriever) < 0
+     "Remise sur CA (Abo. mensuels : #{unit_amount(:subscription)} € x #{quantity_of(:subscription)}, iDofacb. : #{unit_amount(:retriever)} € x #{quantity_of(:retriever)})"
+    elsif unit_amount(:subscription) < 0
+      "Remise sur CA (Abo. mensuels : #{unit_amount(:subscription)} € x #{quantity_of(:subscription)})"
+    elsif unit_amount(:retriever) < 0
+      "Remise sur CA (iDofacb. : #{unit_amount(:retriever)} € x #{quantity_of(:retriever)})"
     else
       "Remise sur CA (-50 dossiers)"
     end
@@ -39,41 +39,59 @@ class DiscountBillingService
   end
 
   def amount_in_cents_of(option)
-    unit_amount[option.to_sym] * quantity_of(option.to_sym) * 100.0
+    unit_amount(option.to_sym) * quantity_of(option.to_sym) * 100.0
   end
 
   def quantity_of(option)
     case option.to_s
       when 'subscription'
-        concerned_subscriptions.size
+        concerned_subscriptions.where("subscriptions.is_basic_package_active = ? OR subscriptions.is_scan_box_package_active = ? OR subscriptions.is_mail_package_active = ?", true, true, true).size
       when 'retriever'
         concerned_subscriptions.where("subscriptions.is_retriever_package_active" => true).size
       else 0
     end
   end
 
-  def subscription_quota
-    quantity_of(:subscription)
-  end
-
-
-  def unit_amount
-    amount = case subscription_quota
-      when (0..50)
-        {subscription: 0, retriever: 0}
-      when (51..100)
-        {subscription: -1, retriever: 0}
-      when (101..200)
-        {subscription: -1.5, retriever: -0.5}
-      when (201..350)
-        {subscription: -2, retriever: -1}
-      when (351..500)
-        {subscription: -3, retriever: -1.25}
-      when (501..Float::INFINITY)
-        {subscription: -4, retriever: -1.5}
+  def unit_amount(option)
+    amount = if apply_special_policy?
+      case quantity_of(option)
+        when (0..50)
+          {subscription: -1, retriever: 0}
+        when (51..150)
+          {subscription: -1.5, retriever: -0.5}
+        when (151..200)
+          {subscription: -2, retriever: -0.75}
+        when (201..250)
+          {subscription: -2.5, retriever: -1}
+        when (251..350)
+          {subscription: -3, retriever: -1.25}
+        when (351..500)
+          {subscription: -4, retriever: -1.50}
+        when (501..Float::INFINITY)
+          {subscription: -5, retriever: -2}
+      end
+    else
+      case quantity_of(option)
+        when (0..50)
+          {subscription: 0, retriever: 0}
+        when (51..100)
+          {subscription: -1, retriever: 0}
+        when (101..200)
+          {subscription: -1.5, retriever: -0.5}
+        when (201..350)
+          {subscription: -2, retriever: -1}
+        when (351..500)
+          {subscription: -3, retriever: -1.25}
+        when (501..Float::INFINITY)
+          {subscription: -4, retriever: -1.5}
+      end
     end
     amount[:retriever] = 0.0 if @organization.subscription.retriever_price_option.to_s == 'reduced_retriever'
-    amount
+    amount[option]
+  end
+
+  def apply_special_policy?
+    %w(GMBA).include? @organization.code
   end
 
   private
@@ -83,7 +101,7 @@ class DiscountBillingService
   end
 
   def concerned_subscriptions
-    @concerned_subscriptions ||= customers.joins(:subscription).where("subscriptions.period_duration" => 1, "subscriptions.is_micro_package_active" => false).where("subscriptions.is_basic_package_active = ? OR subscriptions.is_scan_box_package_active = ?", true, true)
+    @concerned_subscriptions ||= customers.joins(:subscription).where("subscriptions.period_duration" => 1, "subscriptions.is_micro_package_active" => false)
   end
 
 end
--- spec/services/discount_billing_service_spec.rb
+++ spec/services/discount_billing_service_spec.rb
@@ -10,50 +10,29 @@ describe DiscountBillingService do
       Subscription.create(period_duration: 1, organization_id: @organization.id)
     end
 
-    context 'with less than 51 customers' do
+    context "#quantity_of" do
       before(:all) do
+        #basic_package : 10
         1.upto(10).each {
           user = FactoryGirl.create(:user)
           user.options = UserOptions.create(user_id: user.id)
-          subscription = Subscription.create(period_duration: 1, user_id: user.id, is_basic_package_active: true)
+          subscription = Subscription.create(period_duration: 1, user_id: user.id, is_basic_package_active: true, is_retriever_package_active: true)
           @organization.customers << user
           EvaluateSubscription.new(subscription).execute
         }
-        @discount = DiscountBillingService.new(@organization)
-      end
-
-      after(:all) do
-        @organization.customers = []
-      end
-
-      it '#subscription_quota shoud be equal to 10' do
-        expect(@discount.subscription_quota).to eq(10)
-      end
-
-      it '#amount_in_cents of retriever should be equal to 0' do
-        expect(@discount.amount_in_cents_of(:retriever)).to eq(0)
-      end
-
-      it '#amount_in_cents of subscription should be equal to 0' do
-        expect(@discount.amount_in_cents_of(:subscription)).to eq(0)
-      end
-
-      it '#total_amount_in_cents should be equal to 0' do
-        expect(@discount.total_amount_in_cents).to eq(0)
-      end
-
-      it "#title should be correct" do
-        expect(@discount.title).to match(/remise sur ca \(-50 dossiers\)/i)
-      end
-    end
-
-    context 'between [51,100] customers' do
-      before(:all) do
-        #basic_package : 50
-        1.upto(50).each {
+        #scan_box_package : 5
+        1.upto(5).each {
           user = FactoryGirl.create(:user)
           user.options = UserOptions.create(user_id: user.id)
-          subscription = Subscription.create(period_duration: 1, user_id: user.id, is_basic_package_active: true)
+          subscription = Subscription.create(period_duration: 1, user_id: user.id, is_scan_box_package_active: true)
+          @organization.customers << user
+          EvaluateSubscription.new(subscription).execute
+        }
+        #retriever_package only : 3
+        1.upto(3).each {
+          user = FactoryGirl.create(:user)
+          user.options = UserOptions.create(user_id: user.id)
+          subscription = Subscription.create(period_duration: 1, user_id: user.id, is_retriever_package_active: true)
           @organization.customers << user
           EvaluateSubscription.new(subscription).execute
         }
@@ -61,23 +40,15 @@ describe DiscountBillingService do
         1.upto(5).each {
           user = FactoryGirl.create(:user)
           user.options = UserOptions.create(user_id: user.id)
-          subscription = Subscription.create(period_duration: 1, user_id: user.id, is_micro_package_active: true)
+          subscription = Subscription.create(period_duration: 1, user_id: user.id, is_micro_package_active: true, is_retriever_package_active: true)
           @organization.customers << user
           EvaluateSubscription.new(subscription).execute
         }
-        #retriever_package: 5
+        #quaterly period: 5
         1.upto(5).each {
           user = FactoryGirl.create(:user)
           user.options = UserOptions.create(user_id: user.id)
-          subscription = Subscription.create(period_duration: 1, user_id: user.id, is_retriever_package_active: true)
-          @organization.customers << user
-          EvaluateSubscription.new(subscription).execute
-        }
-        #basic package with quaterly period: 5
-        1.upto(5).each {
-          user = FactoryGirl.create(:user)
-          user.options = UserOptions.create(user_id: user.id)
-          subscription = Subscription.create(period_duration: 3, user_id: user.id, is_basic_package_active: true)
+          subscription = Subscription.create(period_duration: 3, user_id: user.id, is_basic_package_active: true, is_retriever_package_active: true)
           @organization.customers << user
           EvaluateSubscription.new(subscription).execute
         }
@@ -89,154 +60,128 @@ describe DiscountBillingService do
         @organization.customers = []
       end
 
-      it '#subscription_quota shoud be equal to 55' do
-        expect(@discount.subscription_quota).to eq(55)
+      it 'subscription should be equal to 15' do
+        expect(@discount.quantity_of(:subscription)).to eq(15)
       end
-
-      it '#quatity_of retriever should be equal to 5' do
-        expect(@discount.quantity_of(:retriever)).to eq(5)
-      end
-
-      it "#amount_in_cents of retriever should be equal to 0" do
-        expect(@discount.amount_in_cents_of(:retriever)).to eq(0)
-      end
-
-      it "#amount_in_cents of subscription should be (1 x 55 x 100) =  -5_500" do
-        expect(@discount.amount_in_cents_of(:subscription)).to eq(-5500.00)
-      end
-
-      it "#total_amount_in_cents should be equal to 5_500" do
-        expect(@discount.total_amount_in_cents).to eq(-5500.00)
-      end
-
-      it "#title should be correct" do
-        expect(@discount.title).to match(/remise sur ca \(abo. mensuels : -1 € x 55\)/i)
+      
+      it 'retriever should be equal to 13' do
+        expect(@discount.quantity_of(:retriever)).to eq(13)
       end
     end
 
-    context 'between [101,200] customers' do
-      before(:each) do
-        @discount = DiscountBillingService.new(@organization)
-        allow(@discount).to receive(:quantity_of).with(:subscription).and_return(105)
-        allow(@discount).to receive(:quantity_of).with(:retriever).and_return(5)
+    context 'quota' do
+      context 'less than 51' do
+        before(:each) do
+          @discount = DiscountBillingService.new(@organization)
+          allow(@discount).to receive(:quantity_of).with(:subscription).and_return(10)
+          allow(@discount).to receive(:quantity_of).with(:retriever).and_return(10)
+        end
+
+        it "#amount_in_cents of subscription should be 0" do
+          expect(@discount.amount_in_cents_of(:subscription)).to eq(0)
+        end
+
+        it "#amount_in_cents of retriever should be 0" do
+          expect(@discount.amount_in_cents_of(:retriever)).to eq(0)
+        end
+
+        it "#title should be correct" do
+          expect(@discount.title).to match(/remise sur CA \(-50 dossiers\)/i)
+        end
       end
 
-      it '#unit_amount should have correct values' do
-        expect(@discount.unit_amount).to eq({subscription: -1.5, retriever: -0.5})
+      context 'subscription = 60 and retriever = 50' do
+        before(:each) do
+          @discount = DiscountBillingService.new(@organization)
+          allow(@discount).to receive(:quantity_of).with(:subscription).and_return(60)
+          allow(@discount).to receive(:quantity_of).with(:retriever).and_return(50)
+        end
+
+        it "#amount_in_cents of subscription should be (1 x 60 x 100) =  -6_000" do
+          expect(@discount.amount_in_cents_of(:subscription)).to eq(-6_000.00)
+        end
+
+        it "#amount_in_cents of retriever should be (0.0 x 50 x 100) =  0" do
+          expect(@discount.amount_in_cents_of(:retriever)).to eq(0)
+        end
+
+        it "#title should be correct" do
+          expect(@discount.title).to match(/remise sur ca \(abo. mensuels : -1 € x 60\)/i)
+        end
       end
 
-      it "#amount_in_cents of retriever should be (0.5 x 5 x 100) =  -250" do
-        expect(@discount.amount_in_cents_of(:retriever)).to eq(-250)
-      end
+      context 'subscription = 45 and retriever = 205' do
+        before(:each) do
+          @discount = DiscountBillingService.new(@organization)
+          allow(@discount).to receive(:quantity_of).with(:subscription).and_return(45)
+          allow(@discount).to receive(:quantity_of).with(:retriever).and_return(205)
+        end
 
-      it "#amount_in_cents of subscription should be (1.5 x 105 x 100) =  -15_750" do
-        expect(@discount.amount_in_cents_of(:subscription)).to eq(-15750.00)
-      end
+        it "#amount_in_cents of subscription should be (0 x 45 x 100) =  0" do
+          expect(@discount.amount_in_cents_of(:subscription)).to eq(0)
+        end
 
-      it "#title should be correct" do
-        expect(@discount.title).to match(/remise sur ca \(abo. mensuels : -1.5 € x 105, idofacb. : -0.5 € x 5\)/i)
+        it "#amount_in_cents of retriever should be (1 x 205 x 100) =  -20_500" do
+          expect(@discount.amount_in_cents_of(:retriever)).to eq(-20_500)
+        end
+
+        it "#title should be correct" do
+          expect(@discount.title).to match(/remise sur ca \(idofacb. : -1 € x 205\)/i)
+        end
       end
     end
 
-    context 'between [201,350] customers' do
-      before(:each) do
-        @discount = DiscountBillingService.new(@organization)
-        allow(@discount).to receive(:quantity_of).with(:subscription).and_return(205)
-        allow(@discount).to receive(:quantity_of).with(:retriever).and_return(5)
+    context 'with special cases : ' do
+      describe 'an organization having a reduced retriever price policy' do
+        before(:each) do
+          @discount = DiscountBillingService.new(@organization)
+          allow(@organization).to receive_message_chain('subscription.retriever_price_option').and_return('reduced_retriever')
+          allow(@discount).to receive(:quantity_of).with(:subscription).and_return(250)
+          allow(@discount).to receive(:quantity_of).with(:retriever).and_return(205)
+        end
+
+        it '#retriever_price_option should return reduced_retriever' do
+          expect(@organization.subscription.retriever_price_option).to eq('reduced_retriever')
+        end
+
+        it "#amount_in_cents of retriever should be (0.0 x 5 x 100) =  0" do
+          expect(@discount.amount_in_cents_of(:retriever)).to eq(0)
+        end
+
+        it "#amount_in_cents of subscription should be (2.0 x 250 x 100) =  -50_000" do
+          expect(@discount.amount_in_cents_of(:subscription)).to eq(-50000.00)
+        end
+
+        it "#title should be correct" do
+          expect(@discount.title).to match(/remise sur ca \(abo. mensuels : -2 € x 250\)/i)
+        end
       end
 
-      it '#unit_amount should have correct values' do
-        expect(@discount.unit_amount).to eq({subscription: -2.0, retriever: -1.0})
+      describe 'an organization having special discount policy' do
+        before(:each) do
+          @discount = DiscountBillingService.new(@organization)
+          @organization.code = 'GMBA'
+          allow(@discount).to receive(:quantity_of).with(:subscription).and_return(45)
+          allow(@discount).to receive(:quantity_of).with(:retriever).and_return(251)
+        end
+
+        it '#apply_special_policy?' do
+          expect(@discount.apply_special_policy?).to be true
+        end
+
+        it "#amount_in_cents of retriever should be (1.25 x 251 x 100) =  -31_375" do
+          expect(@discount.amount_in_cents_of(:retriever)).to eq(-31_375)
+        end
+
+        it "#amount_in_cents of subscription should be (1 x 45 x 100) =  -4_500" do
+          expect(@discount.amount_in_cents_of(:subscription)).to eq(-4_500.00)
+        end
+
+        it "#title should be correct" do
+          expect(@discount.title).to match(/remise sur ca \(abo. mensuels : -1 € x 45, idofacb. : -1.25 € x 251\)/i)
+        end
       end
 
-      it "#amount_in_cents of retriever should be (1.0 x 5 x 100) =  -500" do
-        expect(@discount.amount_in_cents_of(:retriever)).to eq(-500)
-      end
-
-      it "#amount_in_cents of subscription should be (2.0 x 205 x 100) =  -41_000" do
-        expect(@discount.amount_in_cents_of(:subscription)).to eq(-41000.00)
-      end
-
-      it "#title should be correct" do
-        expect(@discount.title).to match(/remise sur ca \(abo. mensuels : -2 € x 205, idofacb. : -1 € x 5\)/i)
-      end
-    end
-
-    context 'between [351,500] customers' do
-      before(:each) do
-        @discount = DiscountBillingService.new(@organization)
-        allow(@discount).to receive(:quantity_of).with(:subscription).and_return(361)
-        allow(@discount).to receive(:quantity_of).with(:retriever).and_return(5)
-      end
-
-      it '#unit_amount should have correct values' do
-        expect(@discount.unit_amount).to eq({subscription: -3.0, retriever: -1.25})
-      end
-
-      it "#amount_in_cents of retriever should be (1.25 x 5 x 100) =  -625" do
-        expect(@discount.amount_in_cents_of(:retriever)).to eq(-625)
-      end
-
-      it "#amount_in_cents of subscription should be (3.0 x 361 x 100) =  -108_300" do
-        expect(@discount.amount_in_cents_of(:subscription)).to eq(-108300.00)
-      end
-
-      it "#title should be correct" do
-        expect(@discount.title).to match(/remise sur ca \(abo. mensuels : -3 € x 361, idofacb. : -1.25 € x 5\)/i)
-      end
-    end
-
-    context 'more than 500 customers' do
-      before(:each) do
-        @discount = DiscountBillingService.new(@organization)
-        allow(@discount).to receive(:quantity_of).with(:subscription).and_return(650)
-        allow(@discount).to receive(:quantity_of).with(:retriever).and_return(5)
-      end
-
-      it '#unit_amount should have correct values' do
-        expect(@discount.unit_amount).to eq({subscription: -4.0, retriever: -1.5})
-      end
-
-      it "#amount_in_cents of retriever should be (1.5 x 5 x 100) =  -750" do
-        expect(@discount.amount_in_cents_of(:retriever)).to eq(-750)
-      end
-
-      it "#amount_in_cents of subscription should be (4.0 x 650 x 100) =  -260_000" do
-        expect(@discount.amount_in_cents_of(:subscription)).to eq(-260000.00)
-      end
-
-      it "#title should be correct" do
-        expect(@discount.title).to match(/remise sur ca \(abo. mensuels : -4 € x 650, idofacb. : -1.5 € x 5\)/i)
-      end
-    end
-
-    context 'with an organization having a reduced retriever price policy' do
-      before(:each) do
-        @discount = DiscountBillingService.new(@organization)
-        allow(@organization).to receive_message_chain('subscription.retriever_price_option').and_return('reduced_retriever')
-        allow(@discount).to receive(:quantity_of).with(:subscription).and_return(250)
-        allow(@discount).to receive(:quantity_of).with(:retriever).and_return(5)
-      end
-
-      it '#retriever_price_option should return reduced_retriever' do
-        expect(@organization.subscription.retriever_price_option).to eq('reduced_retriever')
-      end
-
-      it '#unit_amount should have correct values' do
-        expect(@discount.unit_amount).to eq({subscription: -2.0, retriever: 0.0})
-      end
-
-      it "#amount_in_cents of retriever should be (0.0 x 5 x 100) =  0" do
-        expect(@discount.amount_in_cents_of(:retriever)).to eq(0)
-      end
-
-      it "#amount_in_cents of subscription should be (2.0 x 250 x 100) =  -50_000" do
-        expect(@discount.amount_in_cents_of(:subscription)).to eq(-50000.00)
-      end
-
-      it "#title should be correct" do
-        expect(@discount.title).to match(/remise sur ca \(abo. mensuels : -2 € x 250\)/i)
-      end
     end
   end
 end
