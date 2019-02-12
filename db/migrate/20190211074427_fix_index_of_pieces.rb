7a4ca7295be5c2067e0ed33cd955e70ef5ebdaa3
Mina RAKOTONIRAINY <mina@idocus.com>
2019-02-12 09:26:38 +0300

    Fix index of pieces and preseizures tables

---
+++ db/migrate/20190211074427_fix_index_of_pieces.rb
@@ -0,0 +1,12 @@
+class FixIndexOfPieces < ActiveRecord::Migration
+  def change
+    change_column :pack_pieces, :tags, :text, limit: 65535
+
+    remove_index :pack_pieces, :mongo_id
+    remove_index :pack_pieces, name: "organization_id_mongo_id"
+    remove_index :pack_pieces, name: "pack_id_mongo_id"
+    remove_index :pack_pieces, name: "user_id_mongo_id"
+
+    add_index :pack_pieces, :position
+  end
+end
---
+++ db/migrate/20190211075150_fix_index_of_pack_report_preseizures.rb
@@ -0,0 +1,15 @@
+class FixIndexOfPackReportPreseizures < ActiveRecord::Migration
+  def change
+    change_column :pack_report_preseizures, :operation_label, :text, limit: 65535
+
+    remove_index :pack_report_preseizures, :mongo_id
+    remove_index :pack_report_preseizures, name: 'operation_id_mongo_id'
+    remove_index :pack_report_preseizures, name: 'organization_id_mongo_id'
+    remove_index :pack_report_preseizures, name: 'piece_id_mongo_id'
+    remove_index :pack_report_preseizures, name: 'report_id_mongo_id'
+    remove_index :pack_report_preseizures, name: 'user_id_mongo_id'
+
+    add_index    :pack_report_preseizures, :position
+    add_index    :pack_report_preseizures, :third_party
+  end
+end
