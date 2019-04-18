a9703bd037d15b999378744d24e4b73207e87f5c
Mina RAKOTONIRAINY <mina@idocus.com>
2019-04-18 15:03:49 +0300

    WIP: upgrade rails5 #20

---
+++ db/migrate/20190418140902_restore_ibiza_hash.rb
@@ -0,0 +1,17 @@
+class RestoreIbizaHash < ActiveRecord::Migration[5.2]
+  def up
+    Ibiza.all.each do |i|
+      piece_name_format = i.piece_name_format.to_s
+      description = i.description.to_s
+
+      i.piece_name_format = JSON.parse(piece_name_format.gsub('=>', ':')).to_h
+      i.description = JSON.parse(description.gsub('=>', ':')).to_h
+
+      i.save
+    end
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
