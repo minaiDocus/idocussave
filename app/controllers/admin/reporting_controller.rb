08f61290645666899f17d1f013382f447dec9a54
Mina RAKOTONIRAINY <mina@idocus.com>
2019-06-25 14:41:32 +0300

    Change loading timeout of admin reporting to xls

--- app/controllers/admin/reporting_controller.rb
+++ app/controllers/admin/reporting_controller.rb
@@ -10,28 +10,35 @@ class Admin::ReportingController < Admin::AdminController
     respond_to do |format|
       format.html
       format.xls do
-        if params[:simplified] == '1'
-          filename = "reporting_simplifié_iDocus_#{@year}.xls"
-          send_data GlobalReportToXls.new(@year).execute, type: 'application/vnd.ms-excel', filename: filename
-        else
-          if params[:organization_id].present? && (organization = Organization.find(params[:organization_id]))
-            organization_ids = [organization.id]
-            customer_ids = organization.customers.pluck(:id)
-            filename = "reporting_#{organization.name.downcase.underscore}_#{@year}.xls"
-            with_organization_info = false
-          else
-            organization_ids = @organizations.pluck(:id)
-            customer_ids = @organizations.map { |o| o.customers.pluck(:id) }.flatten
-            filename = "reporting_iDocus_#{@year}.xls"
-            with_organization_info = true
+        begin
+          Timeout::timeout 300 do
+            if params[:simplified] == '1'
+              filename = "reporting_simplifié_iDocus_#{@year}.xls"
+              send_data GlobalReportToXls.new(@year).execute, type: 'application/vnd.ms-excel', filename: filename
+            else
+              if params[:organization_id].present? && (organization = Organization.find(params[:organization_id]))
+                organization_ids = [organization.id]
+                customer_ids = organization.customers.pluck(:id)
+                filename = "reporting_#{organization.name.downcase.underscore}_#{@year}.xls"
+                with_organization_info = false
+              else
+                organization_ids = @organizations.pluck(:id)
+                customer_ids = @organizations.map { |o| o.customers.pluck(:id) }.flatten
+                filename = "reporting_iDocus_#{@year}.xls"
+                with_organization_info = true
+              end
+
+              periods = Period.where("user_id IN (?) OR organization_id IN (?)", customer_ids, organization_ids).
+                where("start_date >= ? AND end_date <= ?", date, date.end_of_year).
+                order(start_date: :asc)
+
+              data = PeriodsToXlsService.new(periods, with_organization_info).execute
+              send_data data, type: 'application/vnd.ms-excel', filename: filename
+            end
           end
-
-          periods = Period.where("user_id IN (?) OR organization_id IN (?)", customer_ids, organization_ids).
-            where("start_date >= ? AND end_date <= ?", date, date.end_of_year).
-            order(start_date: :asc)
-
-          data = PeriodsToXlsService.new(periods, with_organization_info).execute
-          send_data data, type: 'application/vnd.ms-excel', filename: filename
+        rescue Timeout::Error
+          puts 'Request too long'
+          raise RuntimeError.new 'Request too long'
         end
       end
     end
