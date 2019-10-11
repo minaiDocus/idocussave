44f09cec5b5c86e2c435edcd096988ca24a4cf51
Mina RAKOTONIRAINY <mina@idocus.com>
2019-10-11 12:52:08 +0300

    Change debit and credit label to bank accounts rules

--- app/helpers/account/account_number_rules_helper.rb
+++ app/helpers/account/account_number_rules_helper.rb
@@ -2,8 +2,8 @@ module Account::AccountNumberRulesHelper
   def rule_target_options_for_select
     [
       [t('activerecord.models.account_number_rule.attributes.rule_target_values.both'), 'both'],
-      [t('activerecord.models.account_number_rule.attributes.rule_type_values.debit'), 'debit'],
-      [t('activerecord.models.account_number_rule.attributes.rule_type_values.credit'), 'credit']
+      [t('activerecord.models.account_number_rule.attributes.rule_target_values.debit'), 'debit'],
+      [t('activerecord.models.account_number_rule.attributes.rule_target_values.credit'), 'credit']
     ]
   end
 
