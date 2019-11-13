11cb5eb0a9a8c98fc89ba1df1ede08c157c093f7
Mina RAKOTONIRAINY <mina@idocus.com>
2019-11-13 15:28:24 +0300

    Update migrations

--- db/migrate/20191028091005_add_fec_agiris_to_organization.rb
+++ db/migrate/20191028091005_add_fec_agiris_to_organization.rb
@@ -1,4 +1,4 @@
-class AddFecAgirisToOrganization < ActiveRecord::Migration
+class AddFecAgirisToOrganization < ActiveRecord::Migration[5.2]
   def change
   	add_column :organizations, :is_fec_agiris_used, :boolean, default: false
     add_column :organizations, :is_fec_agiris_auto_deliver, :boolean, default: false
--- db/migrate/20191028091032_add_fec_agiris_to_softwares.rb
+++ db/migrate/20191028091032_add_fec_agiris_to_softwares.rb
@@ -1,4 +1,4 @@
-class AddFecAgirisToSoftwares < ActiveRecord::Migration
+class AddFecAgirisToSoftwares < ActiveRecord::Migration[5.2]
   def change
   	add_column :softwares_settings, :is_fec_agiris_used, :boolean, default: false
     add_column :softwares_settings, :is_fec_agiris_auto_deliver, :integer, default: -1, null: false, limit: 4
