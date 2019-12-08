8c4ea9c5ba6c9e3f61347259c68ba0b17e6ee8f1
Thomas ANDRIEU <t.andrieu@gmail.com>
2019-12-08 16:12:19 +0100

    tryfix migrations

--- db/migrate/20190418140902_restore_ibiza_hash.rb
+++ db/migrate/20190418140902_restore_ibiza_hash.rb
@@ -1,5 +1,21 @@
 class RestoreIbizaHash < ActiveRecord::Migration[5.2]
+  class IbizaFix < ApplicationRecord
+    self.table_name = 'ibizas'
+    serialize :description
+    serialize :piece_name_format
+  end
+
   def up
+    IbizaFix.all.each do |m|
+      # Update this to match your real data and what you want `h` to be.
+      pierce_name_format  = m.piece_name_format.to_unsafe_h.to_h
+      m.piece_name_format = pierce_name_format
+
+      description = m.description.to_unsafe_h.to_h
+      m.description = description
+      
+      m.save!
+    end
     Ibiza.all.each do |i|
       piece_name_format = i.piece_name_format.to_s
       description = i.description.to_s
