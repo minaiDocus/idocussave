1ec9446dd0dcdb04124b3ecda8463f34edd7b3c8
Mina RAKOTONIRAINY <mina@idocus.com>
2019-12-17 10:58:34 +0300

    Fix migration target for paper_set_order

--- db/migrate/20191216075023_add_casing_count_to_paper_set_order.rb
+++ db/migrate/20191216075023_add_casing_count_to_paper_set_order.rb
@@ -1,4 +1,4 @@
-class AddCasingCountToPaperSetOrder < ActiveRecord::Migration
+class AddCasingCountToPaperSetOrder < ActiveRecord::Migration[5.2]
   def change
     add_column :orders, :paper_set_casing_count, :integer
   end
