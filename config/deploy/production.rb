31ad066044af3a74a736ece1cef019485b44bfc0
Thomas ANDRIEU <t.andrieu@gmail.com>
2019-12-28 08:44:29 +0100

    readd production deploy config

---
+++ config/deploy/production.rb
@@ -0,0 +1,11 @@
+set :deploy_to, '/data/idocus/deploy/production'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('files')
+
+set :repo_url, 'git@github.com:i-docus/main.git'
+
+set :branch, 'master'
+
+set :rvm_ruby_version, '2.6.5'
+
+server 'legacy.idocus.com', user: 'deploy', roles: %w{app db web}
