0f375a4eff343c399c687641aa0e31235f5e822c
Thomas ANDRIEU <t.andrieu@gmail.com>
2019-12-31 18:41:07 +0100

    do not notify on invoicing

--- app/services/create_invoice_pdf.rb
+++ app/services/create_invoice_pdf.rb
@@ -32,17 +32,17 @@ class CreateInvoicePdf
         CreateInvoicePdf.new(invoice).execute
         print "done\n"
 
-        organization.admins.each do |admin|
-          notification = Notification.new
-          notification.user        = admin
-          notification.notice_type = 'invoice'
-          notification.title       = 'Nouvelle facture disponible'
-          notification.message     = "Votre facture pour le mois de #{I18n.l(invoice.period.start_date, format: '%B')} est maintenant disponible."
-          notification.url         = Rails.application.routes.url_helpers.account_profile_url({ panel: 'invoices' }.merge(ActionMailer::Base.default_url_options))
-          notification.save
-        end
+        #organization.admins.each do |admin|
+        #  notification = Notification.new
+        #  notification.user        = admin
+        #  notification.notice_type = 'invoice'
+        #  notification.title       = 'Nouvelle facture disponible'
+        #  notification.message     = "Votre facture pour le mois de #{I18n.l(invoice.period.start_date, format: '%B')} est maintenant disponible."
+        #  notification.url         = Rails.application.routes.url_helpers.account_profile_url({ panel: 'invoices' }.merge(ActionMailer::Base.default_url_options))
+        #  notification.save
+        #end
 
-        InvoiceMailer.delay(queue: :high).notify(invoice)
+        #InvoiceMailer.delay(queue: :high).notify(invoice)
       end
       Invoice.archive
     end
