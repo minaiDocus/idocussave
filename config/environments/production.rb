8b84a9bfa586364a387219289da859564727043e
Mina RAKOTONIRAINY <mina@idocus.com>
2020-01-13 10:37:00 +0300

    Change log level to :info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Idocus::Application.configure do
   # just comment this out and Rails will serve the files
 
   # See everything in the log (default is :info)
-  config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:uuid]
