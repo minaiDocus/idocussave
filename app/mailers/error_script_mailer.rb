e909f218beda1e9f38fe711f3a200bc227be9feb
Paul <paul@idocus.com>
2020-03-19 12:13:50 +0300

    Log from MCF uploads when it's not persisted

--- app/mailers/error_script_mailer.rb
+++ app/mailers/error_script_mailer.rb
@@ -1,6 +1,12 @@
 class ErrorScriptMailer < ActionMailer::Base
-	def error_notification(script)
+	def error_notification(script, options={})
     @script = script
+
+    if options[:attachements].any?
+      options[:attachements].each do |attach|
+        attachments[attach[:name]] = attach[:file]
+      end
+    end
 
     mail to: "mina@idocus.com,jean@idocus.com,paul@idocus.com", subject: "[MAIL SCRIPT ERREUR] - #{@script[:name]} - #{@script[:erreur_type]}"[0..200]
 	end
