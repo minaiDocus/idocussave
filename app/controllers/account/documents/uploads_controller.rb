d19dbeb3fd1e02bcf46e361be359cb1f0ef6c69f
Paul <paul@idocus.com>
2020-03-24 11:35:34 +0300

    Upload document logger

--- app/controllers/account/documents/uploads_controller.rb
+++ app/controllers/account/documents/uploads_controller.rb
@@ -10,6 +10,14 @@ class Account::Documents::UploadsController < Account::AccountController
                end
 
     if customer.try(:options).try(:is_upload_authorized) && params[:files].present?
+      dir = "#{Rails.root}/files/#{Rails.env}/temp_pack_processor/uploaded_document/"
+
+      FileUtils.makedirs(dir)
+      FileUtils.chmod(0755, dir)
+
+      filename = File.join(dir, "#{customer.code}_#{params[:files][0].original_filename}")
+      FileUtils.copy params[:files][0].tempfile, filename
+
       uploaded_document = UploadedDocument.new(params[:files][0].tempfile,
                                                params[:files][0].original_filename,
                                                customer,
@@ -18,6 +26,24 @@ class Account::Documents::UploadsController < Account::AccountController
                                                current_user,
                                                'web',
                                                params[:analytic])
+
+      if uploaded_document.errors.empty?
+       FileUtils.rm_rf filename
+      else
+        log_document = {
+          name: "Account::Documents::UploadsController",
+          erreur_type: "Uploaded document failed",
+          date_erreur: Time.now.strftime('%Y-%m-%d %H:%M:%S'),
+          more_information: {
+            code: customer.code,
+            customer: customer.inspect,
+            params_file: params[:files][0].inspect,
+            error_reason: uploaded_document.errors.inspect
+          }
+        }
+
+        ErrorScriptMailer.error_notification(log_document).deliver
+      end
 
       data = present(uploaded_document).to_json
     else
