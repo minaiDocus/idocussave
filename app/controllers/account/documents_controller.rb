5b7f728b909656156a268a379447539dca37c745
Mina RAKOTONIRAINY <mina@idocus.com>
2020-04-30 15:27:37 +0300

    Pack archive review

--- app/controllers/account/documents_controller.rb
+++ app/controllers/account/documents_controller.rb
@@ -358,8 +358,8 @@ class Account::DocumentsController < Account::AccountController
   def archive
     pack = Pack.find(params[:id])
     pack = nil unless pack.owner.in?(accounts)
-    zip_path = pack.cloud_archive_object.path.present? ? pack.cloud_archive_object.path : pack.archive_file_path
-    archive_file_name = pack.cloud_archive_object.filename.present? ? pack.cloud_archive_object.filename : pack.archive_name
+    zip_path = pack.cloud_archive_object.path.presence || pack.archive_file_path
+    archive_file_name = pack.cloud_archive_object.filename.presence || pack.archive_name
 
     if pack && File.exist?(zip_path)
       send_file(zip_path, type: 'application/zip', filename: archive_file_name, x_sendfile: true)
