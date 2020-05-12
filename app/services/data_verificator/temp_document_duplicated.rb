052454ce94072a82708feacc705d0d9e02fe8f95
Jean <jean@idocus.com>
2020-05-12 14:20:02 +0300

    Review counter temp document duplicated email view

--- app/services/data_verificator/temp_document_duplicated.rb
+++ app/services/data_verificator/temp_document_duplicated.rb
@@ -5,14 +5,19 @@ class DataVerificator::TempDocumentDuplicated < DataVerificator::DataVerificator
 
     messages = []
 
+    count = 0
+
     _temp_documents = temp_documents.group_by{|element| [element[:original_file_name], element[:pages_number]]}.map{|key, value| key + [value.map(&:original_fingerprint)]}
 
     _temp_documents.each do |temp_document|
-      messages << "original_fine_name: #{temp_document[0]}, pages_number: #{temp_document[1]}, original_fingerprint: #{temp_document[2]}" if temp_document[2].size > 1
+      if temp_document[2].size > 1
+        count += 1
+        messages << "original_fine_name: #{temp_document[0]}, pages_number: #{temp_document[1]}, original_fingerprint: #{temp_document[2].join('|')}"
+      end
     end
 
     {
-      title: "TempDocumentDuplicated - #{_temp_documents.size} TempDocument(s) duplicated original_file_name and pages_number found",
+      title: "TempDocumentDuplicated - #{count} TempDocument(s) duplicated original_file_name and pages_number found",
       type: "table",
       message: messages.join('; ')
     }
