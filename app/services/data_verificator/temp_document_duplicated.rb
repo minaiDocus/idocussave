e57df60d5e2641bc91da493f9d4b43d0fe5c9197
Jean <jean@idocus.com>
2020-05-14 11:25:56 +0300

    count temp document duplicated with original_file_name is present

--- app/services/data_verificator/temp_document_duplicated.rb
+++ app/services/data_verificator/temp_document_duplicated.rb
@@ -10,9 +10,9 @@ class DataVerificator::TempDocumentDuplicated < DataVerificator::DataVerificator
     _temp_documents = temp_documents.group_by{|element| [element[:original_file_name], element[:pages_number]]}.map{|key, value| key + [value.map(&:original_fingerprint)]}
 
     _temp_documents.each do |temp_document|
-      if temp_document[2].size > 1
+      if temp_document[0].present? && temp_document[2].size > 1
         count += 1
-        messages << "original_fine_name: #{temp_document[0]}, pages_number: #{temp_document[1]}, original_fingerprint: #{temp_document[2].join('|')}"
+        messages << "original_file_name: #{temp_document[0]}, count: #{temp_document[2].size}, pages_number: #{temp_document[1]}, original_fingerprint: #{temp_document[2].join('|')}"
       end
     end
 
