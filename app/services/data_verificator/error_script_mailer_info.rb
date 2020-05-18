cdf478af37726d4340862c15f51f8fdc3506a6ff
Mina RAKOTONIRAINY <mina@idocus.com>
2020-05-18 09:46:22 +0300

    check ErrorScriptMailer to block more emails - review

--- app/services/data_verificator/error_script_mailer_info.rb
+++ app/services/data_verificator/error_script_mailer_info.rb
@@ -3,21 +3,18 @@ class DataVerificator::ErrorScriptMailerInfo < DataVerificator::DataVerificator
   def execute
     error_script_mailers = CounterErrorScriptMailer.where("updated_at >= ? AND updated_at <= ?", 1.days.ago, Time.now)
 
-    counter = 0
-
     messages = []
 
     error_script_mailers.each do |error_script_mailer|
-      counter += 1
       messages << "error_type: #{error_script_mailer.error_type}, counter: #{error_script_mailer.counter}, is_enable: #{error_script_mailer.enabled?}"
 
+      error_script_mailer.is_enable = true if error_script_mailer.counter <= 20
       error_script_mailer.counter   = 0
-      error_script_mailer.is_enable = true
       error_script_mailer.save
     end
 
     {
-      title: "ErrorScriptMailerInfo - #{counter} error(s) script mailer found",
+      title: "ErrorScriptMailerInfo - #{error_script_mailers.size} error(s) script mailer found",
       message: messages.join('; ')
     }
   end
