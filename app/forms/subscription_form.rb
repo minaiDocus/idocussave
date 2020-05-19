c56cca5f189c2c9ca1e2331f00c79a7e1f798969
Mina RAKOTONIRAINY <mina@idocus.com>
2020-05-19 12:31:06 +0300

    Fix subscription form params parser

--- app/forms/subscription_form.rb
+++ app/forms/subscription_form.rb
@@ -8,74 +8,74 @@ class SubscriptionForm
   end
 
   def submit(params)
-    dont_apply_now = !(@subscription.user.recently_created? || (@requester.is_admin && params[:is_to_apply_now] == '1'))
+    dont_apply_now = !(@subscription.user.recently_created? || (@requester.is_admin && parse_params(params[:is_to_apply_now]).to_i == 1))
     is_new = !@subscription.configured?
 
     if @subscription.configured?
       if @subscription.light_package?
         if @subscription.is_basic_package_active && dont_apply_now
-          @subscription.is_basic_package_to_be_disabled     = params[:is_basic_package_active]     == '0'
+          @subscription.is_basic_package_to_be_disabled     = parse_params(params[:is_basic_package_active]).to_i     == 0
         else
-          @subscription.is_basic_package_active             = params[:is_basic_package_active]     == '1'
+          @subscription.is_basic_package_active             = parse_params(params[:is_basic_package_active]).to_i     == 1
           if @subscription.is_basic_package_to_be_disabled
             @subscription.is_basic_package_to_be_disabled     = false
           end
         end
 
         if @subscription.is_mail_package_active && dont_apply_now
-          @subscription.is_mail_package_to_be_disabled      = params[:is_mail_package_active]      == '0'
+          @subscription.is_mail_package_to_be_disabled      = parse_params(params[:is_mail_package_active]).to_i      == 0
         else
-          @subscription.is_mail_package_active              = params[:is_mail_package_active]      == '1'
+          @subscription.is_mail_package_active              = parse_params(params[:is_mail_package_active]).to_i      == 1
           if @subscription.is_mail_package_to_be_disabled
             @subscription.is_mail_package_to_be_disabled      = false
           end
         end
 
         if @subscription.is_scan_box_package_active && dont_apply_now
-          @subscription.is_scan_box_package_to_be_disabled  = params[:is_scan_box_package_active]  == '0'
+          @subscription.is_scan_box_package_to_be_disabled  = parse_params(params[:is_scan_box_package_active]).to_i  == 0
         else
-          @subscription.is_scan_box_package_active          = params[:is_scan_box_package_active]  == '1'
+          @subscription.is_scan_box_package_active          = parse_params(params[:is_scan_box_package_active]).to_i  == 1
           if @subscription.is_scan_box_package_to_be_disabled
             @subscription.is_scan_box_package_to_be_disabled  = false
           end
         end
 
         if @subscription.is_retriever_package_active && dont_apply_now
-          @subscription.is_retriever_package_to_be_disabled = params[:is_retriever_package_active] == '0'
+          @subscription.is_retriever_package_to_be_disabled = parse_params(params[:is_retriever_package_active]).to_i == 0
         else
-          @subscription.is_retriever_package_active         = params[:is_retriever_package_active] == '1'
+          @subscription.is_retriever_package_active         = parse_params(params[:is_retriever_package_active]).to_i == 1
           if @subscription.is_retriever_package_to_be_disabled
             @subscription.is_retriever_package_to_be_disabled = false
           end
         end
 
         if @subscription.is_mini_package_active && dont_apply_now
-          @subscription.is_mini_package_to_be_disabled      = params[:is_mini_package_active]      == '0'
+          @subscription.is_mini_package_to_be_disabled      = parse_params(params[:is_mini_package_active]).to_i      == 0
         else
-          @subscription.is_mini_package_active              = params[:is_mini_package_active]      == '1'
+          @subscription.is_mini_package_active              = parse_params(params[:is_mini_package_active]).to_i      == 1
           if @subscription.is_mini_package_to_be_disabled
             @subscription.is_mini_package_to_be_disabled      = false
           end
         end
 
         if @subscription.is_micro_package_active && dont_apply_now
-          @subscription.is_micro_package_to_be_disabled      = params[:is_micro_package_active]      == '0'
+          @subscription.is_micro_package_to_be_disabled      = parse_params(params[:is_micro_package_active]).to_i      == 0
         else
-          @subscription.is_micro_package_active              = params[:is_micro_package_active]      == '1'
+          @subscription.is_micro_package_active              = parse_params(params[:is_micro_package_active]).to_i      == 1
           if @subscription.is_micro_package_to_be_disabled
             @subscription.is_micro_package_to_be_disabled = false
           end
         end
       end
     else
-      if params[:is_annual_package_active] == '1'
+      if params[:is_annual_package_active].to_i == 1
         @subscription.is_annual_package_active    = true
         @subscription.is_basic_package_active     = false
         @subscription.is_mail_package_active      = false
         @subscription.is_scan_box_package_active  = false
         @subscription.is_retriever_package_active = false
         @subscription.period_duration             = 12
-      elsif params[:is_micro_package_active] == '1'
+      elsif params[:is_micro_package_active].to_i == 1
         @subscription.is_micro_package_active     = true
         @subscription.is_annual_package_active    = false
         @subscription.is_mini_package_active      = false
@@ -84,21 +84,21 @@ class SubscriptionForm
         @subscription.is_scan_box_package_active  = false
         @subscription.is_retriever_package_active = false
         @subscription.period_duration             = 1
-      elsif params[:is_mini_package_active] == '1'
+      elsif params[:is_mini_package_active].to_i == 1
         @subscription.is_mini_package_active      = true
         @subscription.is_micro_package_active     = false
         @subscription.is_annual_package_active    = false
         @subscription.is_basic_package_active     = false
         @subscription.is_mail_package_active      = false
         @subscription.is_scan_box_package_active  = false
-        @subscription.is_retriever_package_active = params[:is_retriever_package_active] == '1'
+        @subscription.is_retriever_package_active = parse_params(params[:is_retriever_package_active]).to_i == 1
         @subscription.period_duration             = 1
       else
         @subscription.is_annual_package_active    = false
-        @subscription.is_basic_package_active     = params[:is_basic_package_active]     == '1'
-        @subscription.is_mail_package_active      = params[:is_mail_package_active]      == '1'
-        @subscription.is_scan_box_package_active  = params[:is_scan_box_package_active]  == '1'
-        @subscription.is_retriever_package_active = params[:is_retriever_package_active] == '1'
+        @subscription.is_basic_package_active     = parse_params(params[:is_basic_package_active]).to_i     == 1
+        @subscription.is_mail_package_active      = parse_params(params[:is_mail_package_active]).to_i      == 1
+        @subscription.is_scan_box_package_active  = parse_params(params[:is_scan_box_package_active]).to_i  == 1
+        @subscription.is_retriever_package_active = parse_params(params[:is_retriever_package_active]).to_i == 1
         @subscription.period_duration             = 1
       end
     end
@@ -109,9 +109,9 @@ class SubscriptionForm
 
     if @subscription.is_basic_package_active || @subscription.is_micro_package_active || @subscription.is_mail_package_active || @subscription.is_scan_box_package_active || @subscription.is_mini_package_active
       if @subscription.is_pre_assignment_active && dont_apply_now
-        @subscription.is_pre_assignment_to_be_disabled = params[:is_pre_assignment_active] == 'false'
+        @subscription.is_pre_assignment_to_be_disabled = parse_params(params[:is_pre_assignment_active]).to_i == 0
       else
-        @subscription.is_pre_assignment_active = params[:is_pre_assignment_active] == 'true'
+        @subscription.is_pre_assignment_active = parse_params(params[:is_pre_assignment_active]).to_i == 1
         if @subscription.is_pre_assignment_to_be_disabled
           @subscription.is_pre_assignment_to_be_disabled = false
         end
@@ -167,5 +167,9 @@ class SubscriptionForm
 
       @subscription.assign_attributes(values)
     end
+  end
+
+  def parse_params(pr)
+    pr.to_s.gsub('true', '1').gsub('false', '0')
   end
 end
