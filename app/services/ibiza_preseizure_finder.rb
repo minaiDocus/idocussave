651fa28f52a2fa5acc328c846ef90350c3239ab0
Jean <jean@idocus.com>
2020-05-25 15:51:56 +0300

    Optimize ibiza preseizure finder

--- app/services/ibiza_preseizure_finder.rb
+++ app/services/ibiza_preseizure_finder.rb
@@ -26,36 +26,44 @@ class IbizaPreseizureFinder
   end
 
   def is_delivered?(preseizure)
-    return false
+    client.request.clear
 
-    # client.request.clear
+    account     = preseizure.accounts.first
+    third_party = account.number
 
-    # if preseizure.piece
-    #   search_term = preseizure.piece_number
-    #   search_query_A = 'piece'
-    #   search_query_B = 'voucherRef'
-    # else
-    #   search_term = preseizure.operation_name
-    #   search_query_A ='voucherRef'
-    #   search_query_B ='piece'
-    # end
+    entry  = account.entries.first
+    amount = entry.amount
 
-    # return false if search_term.to_s.size <= 6
+    if entry.type == Pack::Report::Preseizure::Entry::DEBIT
+      entry_type = 'debit'
+    else
+      entry_type = 'credit'
+    end
 
-    # voucher_ref_target = ibiza.try(:voucher_ref_target).presence || 'piece_number'
-    # case voucher_ref_target
-    #   when 'piece_name'
-    #     client.company(user.ibiza_id).grandlivregeneral?("q=#{search_query_A}='#{search_term}'")
-    #   else
-    #     client.company(user.ibiza_id).grandlivregeneral?("q=#{search_query_B}='#{search_term}'")
-    # end
+    if preseizure.piece
+      search_term = preseizure.piece_number
+      search_query_A = 'piece'
+      search_query_B = 'voucherRef'
+    else
+      search_term = preseizure.operation_name
+      search_query_A ='voucherRef'
+      search_query_B ='piece'
+    end
 
-    # if client.response.success?
-    #   response = Nokogiri::XML client.response.body.force_encoding('UTF-8')
-    #   response.at_css('data').children.presence ?  true : false
-    # else
-    #   false
-    # end
+    voucher_ref_target = ibiza.try(:voucher_ref_target).presence || 'piece_number'
+    case voucher_ref_target
+      when 'piece_name'
+        client.company(user.ibiza_id).grandlivregeneral?("q=#{search_query_A}='#{search_term}' and number='#{third_party}' and #{entry_type}='#{amount}'")
+      else
+        client.company(user.ibiza_id).grandlivregeneral?("q=#{search_query_B}='#{search_term}' and number='#{third_party}' and #{entry_type}='#{amount}'")
+    end
+
+    if client.response.success?
+      response = Nokogiri::XML client.response.body.force_encoding('UTF-8')
+      response.at_css('data').children.presence ?  true : false
+    else
+      false
+    end
   end
 
   private
