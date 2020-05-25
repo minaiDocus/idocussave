7d929b8f83a8114be833138a7157784d0933b15d
Mina RAKOTONIRAINY <mina@idocus.com>
2020-05-25 15:47:35 +0300

    Migrate extentis customers to iDo Micro - review

--- app/services/ponctual_scripts/migrate_customers_subscriptions.rb
+++ app/services/ponctual_scripts/migrate_customers_subscriptions.rb
@@ -10,27 +10,14 @@ class PonctualScripts::MigrateCustomersSubscriptions < PonctualScripts::Ponctual
   private 
 
   def execute
-    tab_customers = get_customers
+    get_customers.each do |customer|
+      customer = customer.strip
+      subscription = get_user(customer).try(:subscription)
 
-    tab_customers.each do |customer|
-      subscription = Subscription.where(user_id: get_user_id(customer)).first  
-
-      params_backup                                        = {}
-      params_backup['id']                                  = subscription.id
-      params_backup['is_basic_package_active']             = subscription.is_basic_package_active
-      params_backup['is_mail_package_active']              = subscription.is_mail_package_active
-      params_backup['is_scan_box_package_active']          = subscription.is_scan_box_package_active
-      params_backup['is_retriever_package_active']         = subscription.is_retriever_package_active
-      params_backup['is_mini_package_active']              = subscription.is_mini_package_active
-      params_backup['is_annual_package_active']            = subscription.is_annual_package_active
-      params_backup['is_basic_package_to_be_disabled']     = subscription.is_basic_package_to_be_disabled
-      params_backup['is_mail_package_to_be_disabled']      = subscription.is_mail_package_to_be_disabled
-      params_backup['is_scan_box_package_to_be_disabled']  = subscription.is_scan_box_package_to_be_disabled
-      params_backup['is_retriever_package_to_be_disabled'] = subscription.is_retriever_package_to_be_disabled
-      params_backup['is_pre_assignment_to_be_disabled']    = subscription.is_pre_assignment_to_be_disabled
-      params_backup['is_micro_package_to_be_disabled']     = subscription.is_micro_package_to_be_disabled
-      params_backup['is_micro_package_active']             = subscription.is_micro_package_active
-      params_backup['is_pre_assignment_active']            = subscription.is_pre_assignment_active
+      if !subscription || subscription.try(:heavy_package?)
+        logger_infos "[MigrateCustomersSubscriptions] - customer: #{customer.to_s} - subscription ID : #{subscription.id.to_s} - can't be updated"
+        next
+      end
 
       params_update                                       = {}
       params_update[:is_basic_package_active]             = false
@@ -39,57 +26,36 @@ class PonctualScripts::MigrateCustomersSubscriptions < PonctualScripts::Ponctual
       params_update[:is_retriever_package_active]         = false
       params_update[:is_mini_package_active]              = false
       params_update[:is_annual_package_active]            = false
-      params_update[:is_basic_package_to_be_disabled]     = false
-      params_update[:is_mail_package_to_be_disabled]      = false
-      params_update[:is_scan_box_package_to_be_disabled]  = false
-      params_update[:is_retriever_package_to_be_disabled] = false
-      params_update[:is_micro_package_to_be_disabled]     = false
       params_update[:is_micro_package_active]             = true
       params_update[:is_pre_assignment_active]            = subscription.is_pre_assignment_active
-      params_update[:is_pre_assignment_to_be_disabled]    = subscription.is_retriever_package_to_be_disabled
+
+      File.write(File.join(ponctual_dir, "#{customer}.json"), subscription.to_json)
 
       SubscriptionForm.new(subscription, requester).submit(params_update)
-
-      File.write(File.join(ponctual_dir, "#{customer}.json"), params_backup.to_json)
     end
   end
 
   def backup
-    tab_customers = get_customers
-
-    tab_customers.each do |customer|
+    get_customers.each do |customer|
+      customer = customer.strip
       params = JSON.parse(File.read(File.join(ponctual_dir, "#{customer}.json"))).with_indifferent_access
 
       subscription = Subscription.find params[:id]
 
-      subscription.is_basic_package_active             = params[:is_basic_package_active]
-      subscription.is_mail_package_active              = params[:is_mail_package_active]
-      subscription.is_scan_box_package_active          = params[:is_scan_box_package_active]
-      subscription.is_retriever_package_active         = params[:is_retriever_package_active]
-      subscription.is_mini_package_active              = params[:is_mini_package_active]
-      subscription.is_annual_package_active            = params[:is_annual_package_active]
-      subscription.is_basic_package_to_be_disabled     = params[:is_basic_package_to_be_disabled]
-      subscription.is_mail_package_to_be_disabled      = params[:is_mail_package_to_be_disabled]
-      subscription.is_scan_box_package_to_be_disabled  = params[:is_scan_box_package_to_be_disabled]
-      subscription.is_retriever_package_to_be_disabled = params[:is_retriever_package_to_be_disabled]
-      subscription.is_pre_assignment_to_be_disabled    = params[:is_pre_assignment_to_be_disabled]
-      subscription.is_micro_package_to_be_disabled     = params[:is_micro_package_to_be_disabled]
-      subscription.is_micro_package_active             = params[:is_micro_package_active]
-      subscription.is_pre_assignment_active            = params[:is_pre_assignment_active]
-
+      subscription.assign_attributes(params)
 
       if subscription.save
         EvaluateSubscription.new(subscription, requester).execute
 
         UpdatePeriod.new(subscription.current_period).execute
       else
-         logger_infos "[MigrateCustomersSubscriptions] - customer: #{customer.to_s} - subscription ID : #{subscription.id.to_s}"
+         logger_infos "[MigrateCustomersSubscriptions] - customer: #{customer.to_s} - subscription ID : #{subscription.id.to_s} - rollback failed"
       end
     end
   end
 
-  def get_user_id(code)
-    User.find_by_code(code).try(:id) || nil
+  def get_user(code)
+    User.find_by_code(code.strip) || nil
   end
 
   def ponctual_dir
