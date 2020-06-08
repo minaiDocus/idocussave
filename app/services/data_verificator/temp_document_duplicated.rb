6c1d75a9cb0db0d1c959178522be11fa38ed9d7d
Jean <jean@idocus.com>
2020-06-08 10:09:48 +0300

    Miscelaneous changes -- dataverif

--- app/services/data_verificator/temp_document_duplicated.rb
+++ app/services/data_verificator/temp_document_duplicated.rb
@@ -2,7 +2,7 @@
 class DataVerificator::TempDocumentDuplicated < DataVerificator::DataVerificator
   def execute
     mcn_organization = Organization.find_by_code 'MCN'
-    temp_documents = TempDocument.where('updated_at >= ? AND updated_at <= ? AND organization_id != ?', 2.days.ago, Time.now, mcn_organization.id).distinct.select(:original_file_name, :pages_number, :user_id)
+    temp_documents = TempDocument.where('updated_at >= ? AND updated_at <= ? AND organization_id != ?', 1.days.ago, Time.now, mcn_organization.id).distinct.select(:original_file_name, :pages_number, :user_id)
 
     messages = []
 
@@ -11,12 +11,12 @@ class DataVerificator::TempDocumentDuplicated < DataVerificator::DataVerificator
     temp_documents.each do |temp_document_info|
       raw_temp_documents = TempDocument.where('updated_at >= ? AND updated_at <= ? AND original_file_name = ? AND pages_number = ? AND user_id = ?', 2.months.ago, Time.now, temp_document_info.original_file_name, temp_document_info.pages_number, temp_document_info.user_id)
       if raw_temp_documents.size > 1
-        _temp_document = raw_temp_documents.group_by{|element| [element[:original_file_name], element[:pages_number]]}.map{|key, value| key + [value.map{|p| [p.created_at.strftime('%d-%m-%Y %H:%M:%S'), p.api_name, p.content_file_name, p.original_fingerprint]}]}.first
+        temp_document = raw_temp_documents.group_by{|element| [element[:original_file_name], element[:pages_number]]}.map{|key, value| key + [value.map{|p| [p.id, p.state, p.created_at.strftime('%d-%m-%Y %H:%M:%S'), p.api_name, p.content_file_name, p.original_fingerprint]}]}.first
         tmp_doc_elements = []
-        if _temp_document[0].present? && _temp_document[2].size > 1
-          _temp_document[2].each{|tmp_doc_el| tmp_doc_elements << tmp_doc_el.join(' => ')}
+        if temp_document[0].present? && temp_document[2].size > 1
+          temp_document[2].each{|tmp_doc_el| tmp_doc_elements << tmp_doc_el.join(' => ')}
           count += 1
-          messages << "user_code: #{raw_temp_documents.first.user.code}, original_file_name: #{_temp_document[0]}, count: #{_temp_document[2].size}, pages_number: #{_temp_document[1]}, details: #{tmp_doc_elements.join('<br/>')}"
+          messages << "user_code: #{raw_temp_documents.first.user.code}, original_file_name: #{temp_document[0]}, count: #{temp_document[2].size}, pages_number: #{temp_document[1]}, details: #{tmp_doc_elements.join('<br/>')}"
         end
       end
     end
