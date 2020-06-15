1845f9fcd3b52d81fac964069c57a17a2a719351
Paul <paul@idocus.com>
2020-06-15 08:43:51 +0300

    Api- SGI : send piece to Preassignment

---
+++ spec/services/sgi_api_services/retrieve_pre_asignment_service_spec.rb
@@ -0,0 +1,18 @@
+# -*- encoding : UTF-8 -*-
+require 'spec_helper'
+
+describe SgiApiServices::GroupDocument do
+  before(:all) do
+      Timecop.freeze(Time.local(2020,06,12,0,1,0))
+  end
+
+  after(:all) do
+    Timecop.return
+  end
+
+  describe '' do
+    it ' ' do
+      
+    end
+  end
+end
