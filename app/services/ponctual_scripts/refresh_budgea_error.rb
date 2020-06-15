c792cae35fcb739f69a8ad29eab592e0a166a4a3
Paul <paul@idocus.com>
2020-06-15 16:20:14 +0300

    Ponctual script for fix budgea error SCArequired and decoupled

---
+++ app/services/ponctual_scripts/refresh_budgea_error.rb
@@ -0,0 +1,29 @@
+class PonctualScripts::RefreshBudgeaError
+  def initialize(retrievers)
+    @retrievers = retrievers
+  end
+
+  def execute
+    counter = 0
+
+    @retrievers.each_with_index do |retriever, index|
+      if counter >= 5
+        sleep(10)
+        counter = 0
+      end
+
+      next if !retriever.budgea_error_message.in? ['SCARequired', 'decoupled']
+
+      infos = "#{retriever.id.to_s} ******** #{retriever.budgea_error_message.to_s} ******** #{index} ******** "
+      p infos
+
+      connection = { error: retriever.budgea_error_message }
+      retriever.update_state_with(connection)
+
+      infos += retriever.reload.budgea_error_message.to_s
+      LogService.info('ponctual_scripts_budgea_error', infos)
+
+      counter += 1
+    end
+  end
+end
