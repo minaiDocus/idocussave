522d9878858c1dcc85034df1440b47ce83c340eb
Paul <paul@idocus.com>
2020-09-30 16:52:42 +0300

    Api- SGI : send piece to Preassignment next changes

--- app/services/sgi_api_services/retrieve_pre_asignment_service.rb
+++ app/services/sgi_api_services/retrieve_pre_asignment_service.rb
@@ -1,19 +1,23 @@
 # -*- encoding : UTF-8 -*-
 class SgiApiServices::RetrievePreAsignmentService
-  def initialize(preassignment)
-    @preassignment = preassignment
+  def initialize(data_preassignment)
+    @pack_preassignment = data_preassignment["packs"]
   end
 
   def execute
-    @preassignment.each do |pack_id, data_pieces|
-      pack     = Pack.find pack_id
-      next unless pack
+    @list_pieces = []
+
+    @pack_preassignment.each do |data_pack|
+      pack     = Pack.find data_pack["id"]
+      next unless pack.present?
+
+      @process = data_pack["process"]
 
       period   = pack.owner.subscription.find_or_create_period(Date.today)
       document = Reporting.find_or_create_period_document(pack, period)
       report   = document.report || create_report(pack, document)
 
-      pre_assignments = get_pre_assignments(data_pieces, report)
+      pre_assignments = get_pre_assignments(data_pack["pieces"], report)
 
       UpdatePeriodDataService.new(period).execute
       UpdatePeriodPriceService.new(period).execute
@@ -32,6 +36,8 @@ class SgiApiServices::RetrievePreAsignmentService
         FileDelivery.prepare(pack)
       end
     end
+
+    @list_pieces.flatten
   end
 
   private
@@ -46,49 +52,46 @@ class SgiApiServices::RetrievePreAsignmentService
 
   def get_pre_assignments(data_pieces, report)
     pre_assignments = []
-    data_pieces.each do |datas|
-      piece   = Pack::Piece.where(name: datas['piece_name'].tr('_', ' ')).first
+
+    data_pieces.each do |data_piece|
+      _ignored = false
+      piece = Pack::Piece.find data_piece["id"]
       journal = piece.user.account_book_types.where(name: piece.pack.name.split[1]).first if piece
 
       errors = []
-      errors << "Piece #{xml_piece['name']} unknown or deleted" unless piece
+      errors << "Piece #{data_piece['name']} unknown or deleted" unless piece
       errors << "Journal not found" unless journal
-      errors << "Piece #{xml_piece['name']} already pre-assigned" if piece && piece.is_already_pre_assigned_with?(@process)
+      errors << "Piece #{data_piece['name']} already pre-assigned" if piece && piece.is_already_pre_assigned_with?(@process)
 
       if errors.empty?
-         _ignored = false
-
         if is_preseizure?
-          _ignoring_reason = datas['ignore'].try(:content).to_s.presence
+          _ignoring_reason = data_piece['ignore'].to_s.presence
 
           if _ignoring_reason.present?
             _ignored = true
 
             NotifyPreAssignmentIgnoredPiece.new(piece, 5.minutes).execute unless piece.is_deleted?
           else
-            datas['preseizure'].each do |data|
+            data_piece['preseizure'].each do |data|
               pre_assignments << create_preseizure(piece, report, data)
             end
           end
-
         elsif is_expense?
-          pre_assignments << create_expense(piece, report, xml_piece)
+          pre_assignments << create_expense(piece, report, data)
         end
 
         _ignored ? piece.ignored_pre_assignment : piece.processed_pre_assignment
-        piece.update(is_awaiting_pre_assignment: false, pre_assignment_comment: _ignoring_reason)        
-      else
-        if piece
-          piece.update(is_awaiting_pre_assignment: false)
-          piece.not_processed_pre_assignment
-        end
-        nil
+      elsif piece
+        piece.not_processed_pre_assignment
       end
+
+      piece.update(is_awaiting_pre_assignment: false, pre_assignment_comment: _ignoring_reason) if piece
+
+      @list_pieces << { id: piece.id, name: piece.name, errors: errors }
     end
 
     pre_assignments
   end
-
 
   def create_preseizure(piece, report, data)
     preseizure = Pack::Report::Preseizure.new
@@ -96,32 +99,33 @@ class SgiApiServices::RetrievePreAsignmentService
     preseizure.piece            = piece
     preseizure.user             = piece.user
     preseizure.organization     = piece.user.organization
-    preseizure.piece_number     = data['piece_number'].try(:content)
-    preseizure.amount           = to_float(data['amount'].try(:content))
-    preseizure.currency         = data['currency'].try(:content)
-    preseizure.unit             = data['unit'].try(:content)
-    preseizure.conversion_rate  = to_float(data['conversion_rate'].try(:content))
-    preseizure.third_party      = data['third_party'].try(:content)
-    preseizure.date             = data['date'].try(:content).try(:to_date)
-    preseizure.deadline_date    = data['deadline_date'].try(:content).try(:to_date)
-    preseizure.observation      = data['observation'].try(:content)
+    preseizure.piece_number     = data['piece_number']
+    preseizure.amount           = data['amount'].try(:to_f)
+    preseizure.currency         = data['currency']
+    preseizure.unit             = data['unit']
+    preseizure.conversion_rate  = data['conversion_rate'].try(:to_f)
+    preseizure.third_party      = data['third_party']
+    preseizure.date             = data['date'].try(:to_date)
+    preseizure.deadline_date    = data['deadline_date'].try(:to_date)
+    preseizure.observation      = data['observation']
     preseizure.position         = piece.position
-    preseizure.is_made_by_abbyy = data['is_made_by_abbyy'].try(:content)
+    preseizure.is_made_by_abbyy = data['is_made_by_abbyy']
     preseizure.save
 
-    data['account'].each do |xml_account|
-      account = Pack::Report::Preseizure::Account.new
-      account.type      = Pack::Report::Preseizure::Account.get_type(xml_account['type'])
-      account.number    = xml_account['number']
-      account.lettering = xml_account['lettering']
+    data['account'].each do |data_account|
+      account           = Pack::Report::Preseizure::Account.new
+      account.type      = Pack::Report::Preseizure::Account.get_type(data_account['type'])
+      account.number    = data_account['number']
+      account.lettering = data_account['lettering']
       account.save
+
       preseizure.accounts << account
 
-      xml_account['debit,credit'].each do |xml_entity|
+      data_account['debit,credit'].each do |data_entity|
         entry = Pack::Report::Preseizure::Entry.new
-        entry.type   = "Pack::Report::Preseizure::Entry::#{xml_entity.name.upcase}".constantize
-        entry.number = xml_entity['number'].to_i
-        entry.amount = to_float(xml_entity.content)
+        entry.type   = "Pack::Report::Preseizure::Entry::#{data_entity["name"].upcase}".constantize
+        entry.number = data_entity['number'].to_i
+        entry.amount = data_entity["value"].try(:to_f)
         entry.save
         account.entries << entry
         preseizure.entries << entry
@@ -144,23 +148,23 @@ class SgiApiServices::RetrievePreAsignmentService
     expense.piece                  = piece
     expense.user                   = report.user
     expense.organization           = report.organization
-    expense.amount_in_cents_wo_vat = to_float(data['ht'].try(:content))
-    expense.amount_in_cents_w_vat  = to_float(data['ttc'].try(:content))
-    expense.vat                    = to_float(data['tva'].try(:content))
-    expense.date                   = data['date'].try(:content).try(:to_date)
-    expense.type                   = data['type'].try(:content)
-    expense.origin                 = data['source'].try(:content)
+    expense.amount_in_cents_wo_vat = data['ht'].try(:to_f)
+    expense.amount_in_cents_w_vat  = data['ttc'].try(:to_f)
+    expense.vat                    = data['tva'].try(:to_f)
+    expense.date                   = data['date'].try(:to_date)
+    expense.type                   = data['type']
+    expense.origin                 = data['source']
     expense.obs_type               = obs['type'].to_i
     expense.position               = piece.position
     expense.save
     observation         = Pack::Report::Observation.new
     observation.expense = expense
-    observation.comment = obs['observation'].try(:content)
+    observation.comment = obs['observation']
     observation.save
 
     obs['guest'].each do |guest|
-      first_name = guest['first_name'].first.try(:content)
-      last_name  = guest['last_name'].first.try(:content)
+      first_name = guest['first_name'].first
+      last_name  = guest['last_name'].first
       next unless first_name.present? || last_name.present?
       g = Pack::Report::Observation::Guest.new
       g.observation = observation
