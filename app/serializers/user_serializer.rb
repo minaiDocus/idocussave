ca8564450605e810aa06c16d77f4a17a1b6f3674
Thomas ANDRIEU <t.andrieu@gmail.com>
2020-11-11 18:50:45 +0100

    improve user seralizer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :jefacture_account_id, :jefacture_api_key
+  attributes :id, :jefacture_account_id, :jefacture_api_key, :code
 end
