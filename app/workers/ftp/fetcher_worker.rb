9bf8c44c65128f54934010b2d6da8f576dde6654
Jean <jean@idocus.com>
2020-11-11 09:08:42 +0300

    Restructure ftp services

--- app/workers/ftp/fetcher_worker.rb
+++ app/workers/ftp/fetcher_worker.rb
@@ -1,11 +1,11 @@
-class FtpFetcherWorker
+class Ftp::FetcherWorker
   include Sidekiq::Worker
   sidekiq_options retry: false, unique: :until_and_while_executing
 
   def perform
     if FTPDeliveryConfiguration::IS_ENABLED
       UniqueJobs.for 'FtpFetcher' do
-        FtpFetcher.fetch(FTPDeliveryConfiguration::FTP_SERVER,
+        Ftp::Fetcher.fetch(FTPDeliveryConfiguration::FTP_SERVER,
                          FTPDeliveryConfiguration::FTP_USERNAME,
                          FTPDeliveryConfiguration::FTP_PASSWORD,
                          FTPDeliveryConfiguration::FTP_PATH,
