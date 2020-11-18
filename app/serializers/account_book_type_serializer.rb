d6f423b0965f350d5d831383ab624b5805370e02
Thomas ANDRIEU <t.andrieu@gmail.com>
2020-11-18 10:30:02 +0100

    added fields in serializers

--- app/serializers/account_book_type_serializer.rb
+++ app/serializers/account_book_type_serializer.rb
@@ -1,5 +1,5 @@
 class AccountBookTypeSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :user_id, :name, :description, :entry_type, :jefacture_enabled, :charge_account,
-             :default_charge_account, :account_number, :default_account_number, :anomaly_account
+             :default_charge_account, :account_number, :default_account_number, :anomaly_account, :vat_account
 end
--- app/serializers/accounting_plan_item_serializer.rb
+++ app/serializers/accounting_plan_item_serializer.rb
@@ -1,5 +1,5 @@
 class AccountingPlanItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :third_party_name, :third_party_account, :conterpart_account, :vat_autoliquidation,
+  attributes :id, :third_party_name, :third_party_account, :conterpart_account, :vat_autoliquidation, :code,
              :vat_autoliquidation_credit_account, :vat_autoliquidation_debit_account
 end
