7e6e81037f736999739fe7bf037794a85f0b8041
Paul <paul@idocus.com>
2020-11-27 13:08:20 +0300

    MyUnisoft : Pre-assignment

---
+++ app/services/pre_assignment/builder/my_unisoft.rb
@@ -0,0 +1,27 @@
+class PreAssignment::Builder::MyUnisoft < PreAssignment::Builder::DataService
+  def self.execute(delivery)
+    new(delivery).run
+  end
+
+  def initialize(delivery)
+    super
+
+    @software = @delivery.user.my_unisoft
+  end
+
+  private
+
+  def execute
+    @delivery.building_data
+
+    response = MyUnisoft::DataBuilder.new(@preseizures).execute
+
+    if response[:data_built]
+      save_data_to_storage(response[:data], 'txt')
+      
+      building_success response[:data_count]
+    else
+      building_failed response[:error_messages]
+    end
+  end
+end
