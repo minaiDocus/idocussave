ad2883171f5f281e1c1c4c031efe0330a707d249
Paul <paul@idocus.com>
2021-02-04 13:16:45 +0300

    Subscription: ponctual script migration modified

--- app/services/ponctual_scripts/migrate_subscription_packages.rb
+++ app/services/ponctual_scripts/migrate_subscription_packages.rb
@@ -13,27 +13,20 @@ class PonctualScripts::MigrateSubscriptionPackages < PonctualScripts::PonctualSc
     Subscription.all.each do |subscription|
       next if subscription.current_packages.present?
 
-      current_package = []
       futur_package   = []
 
-      current_package << 'ido_x'                   if subscription.is_idox_package_active
-      current_package << 'ido_micro'               if subscription.is_micro_package_active
-      current_package << 'ido_mini'                if subscription.is_mini_package_active
-      current_package << 'ido_classique'           if subscription.is_basic_package_active
-      current_package << 'mail_option'             if subscription.is_mail_package_active
-      current_package << 'retriever_option'        if subscription.is_retriever_package_active
-      current_package << 'pre_assignment_option'   if subscription.is_pre_assignment_active
+      period_packages = subscription.current_period.current_packages
 
-      futur_package << 'ido_x'                     if subscription.is_idox_package_active && !subscription.is_idox_package_to_be_disabled
-      futur_package << 'ido_micro'                 if subscription.is_micro_package_active && !subscription.is_micro_package_to_be_disabled
-      futur_package << 'ido_mini'                  if subscription.is_mini_package_active && !subscription.is_mini_package_to_be_disabled
-      futur_package << 'ido_classique'             if subscription.is_basic_package_active && !subscription.is_basic_package_to_be_disabled
-      futur_package << 'mail_option'               if subscription.is_mail_package_active && !subscription.is_mail_package_to_be_disabled
-      futur_package << 'retriever_option'          if subscription.is_retriever_package_active && !subscription.is_retriever_package_to_be_disabled
-      futur_package << 'pre_assignment_option'     if subscription.is_pre_assignment_active && !subscription.is_pre_assignment_to_be_disabled
+      futur_package << 'ido_x'                     if subscription.is_idox_package_active && !period_packages.include?('ido_x') && !subscription.is_idox_package_to_be_disabled
+      futur_package << 'ido_micro'                 if subscription.is_micro_package_active && !period_packages.include?('ido_micro') && !subscription.is_micro_package_to_be_disabled
+      futur_package << 'ido_mini'                  if subscription.is_mini_package_active && !period_packages.include?('ido_mini') && !subscription.is_mini_package_to_be_disabled
+      futur_package << 'ido_classique'             if subscription.is_basic_package_active && !period_packages.include?('ido_classique') && !subscription.is_basic_package_to_be_disabled
+      futur_package << 'mail_option'               if subscription.is_mail_package_active && !period_packages.include?('mail_option') && !subscription.is_mail_package_to_be_disabled
+      futur_package << 'retriever_option'          if subscription.is_retriever_package_active && !period_packages.include?('retriever_option') && !subscription.is_retriever_package_to_be_disabled
+      futur_package << 'pre_assignment_option'     if subscription.is_pre_assignment_active && !period_packages.include?('pre_assignment_option') && !subscription.is_pre_assignment_to_be_disabled
 
-      subscription.current_packages = current_package
-      subscription.futur_packages   = futur_package if current_package.uniq != futur_package.uniq
+      subscription.current_packages = period_packages
+      subscription.futur_packages   = futur_package.any? ? futur_package : nil
 
       subscription.save
     end
