ad291864b9d0d81c213e24a1f3728d6cdcc1794b
Mina RAKOTONIRAINY <mina@idocus.com>
2021-02-04 13:36:35 +0300

    Migrate subscription --- review

--- app/services/ponctual_scripts/migrate_subscription_packages.rb
+++ app/services/ponctual_scripts/migrate_subscription_packages.rb
@@ -15,15 +15,17 @@ class PonctualScripts::MigrateSubscriptionPackages < PonctualScripts::PonctualSc
 
       futur_package   = []
 
-      period_packages = subscription.current_period.current_packages
+      next unless subscription.user && subscription.user.still_active?
+
+      period_packages = subscription.periods.last.try(:current_packages).presence || []
 
       futur_package << 'ido_x'                     if subscription.is_idox_package_active && !period_packages.include?('ido_x') && !subscription.is_idox_package_to_be_disabled
       futur_package << 'ido_micro'                 if subscription.is_micro_package_active && !period_packages.include?('ido_micro') && !subscription.is_micro_package_to_be_disabled
       futur_package << 'ido_mini'                  if subscription.is_mini_package_active && !period_packages.include?('ido_mini') && !subscription.is_mini_package_to_be_disabled
       futur_package << 'ido_classique'             if subscription.is_basic_package_active && !period_packages.include?('ido_classique') && !subscription.is_basic_package_to_be_disabled
-      futur_package << 'mail_option'               if subscription.is_mail_package_active && !period_packages.include?('mail_option') && !subscription.is_mail_package_to_be_disabled
-      futur_package << 'retriever_option'          if subscription.is_retriever_package_active && !period_packages.include?('retriever_option') && !subscription.is_retriever_package_to_be_disabled
-      futur_package << 'pre_assignment_option'     if subscription.is_pre_assignment_active && !period_packages.include?('pre_assignment_option') && !subscription.is_pre_assignment_to_be_disabled
+      futur_package << 'mail_option'               if subscription.is_mail_package_active && !subscription.is_mail_package_to_be_disabled
+      futur_package << 'retriever_option'          if subscription.is_retriever_package_active && !subscription.is_retriever_package_to_be_disabled
+      futur_package << 'pre_assignment_option'     if subscription.is_pre_assignment_active && !subscription.is_pre_assignment_to_be_disabled
 
       subscription.current_packages = period_packages
       subscription.futur_packages   = futur_package.any? ? futur_package : nil
