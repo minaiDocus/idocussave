435a304b703e9c08028ad4931ad42156acf250ac
Mina RAKOTONIRAINY <mina@idocus.com>
2021-02-23 15:23:36 +0300

    Resend blocked operation script - review

--- app/services/ponctual_scripts/resend_operation_blocked.rb
+++ app/services/ponctual_scripts/resend_operation_blocked.rb
@@ -6,16 +6,23 @@ class PonctualScripts::ResendOperationBlocked < PonctualScripts::PonctualScript
   private
 
   def execute
-    operations = Operation.where('created_at BETWEEN "2021-02-19 00:00:00" AND "2021-02-23 12:25:00" AND api_name = "capidocus"').collect(&:api_id)
+    api_ids = Operation.where('created_at BETWEEN "2021-02-19 00:00:00" AND "2021-02-22 23:59:59" AND api_name = "capidocus"').collect(&:api_id).uniq
     @result    = { created_operation: [], created_operation_count: 0, rejected_operation: [], rejected_operation_count: 0 }
 
-    logger_infos "[ResendOperationBlocked] - total operation need to resend: #{operations.size}"
+    logger_infos "[ResendOperationBlocked] - total piece need to resend: #{api_ids.size}"
 
-    operations.each do |api_id|
+    api_ids.each do |api_id|
+      next if api_id == 'JNT%DINU_BQ_202102_004'
+
       logger_infos "[ResendOperationBlocked] - with api_id: #{api_id} - Start"
 
-      content  = File.read("/nfs/staffing_internal/livraison/pre_assigning/#{api_id}.json")
-      assign_data(JSON.parse(content))
+      file_p = "/nfs/staffing_internal/livraison/pre_assigning/#{api_id}.json"
+      if File.exist?(file_p)
+        content  = File.read(file_p)
+        assign_data(JSON.parse(content))
+      else
+        logger_infos "[ResendOperationBlocked] - #{api_id} - doesn't exist"
+      end
 
       logger_infos "[ResendOperationBlocked] - with api_id: #{api_id} - Finished"
     end
@@ -34,7 +41,7 @@ class PonctualScripts::ResendOperationBlocked < PonctualScripts::PonctualScript
     }
 
     begin
-      ErrorScriptMailer.error_notification(mail_infos, { attachements: [{name: 'result.json', file: File.read(file_path)}] } ).deliver
+      ErrorScriptMailer.error_notification(mail_infos, { attachements: [{name: 'operations_results.json', file: File.read(file_path)}] } ).deliver
     rescue
       ErrorScriptMailer.error_notification(mail_infos).deliver
     end
@@ -44,47 +51,53 @@ class PonctualScripts::ResendOperationBlocked < PonctualScripts::PonctualScript
     operations.each do |operation|
       @bank_account = BankAccount.find(operation[:bank_account_id])
 
-      if @bank_account.operations.where(amount: operation[:amount], date: operation[:date], api_name: 'capidocus').size > 0
-        logger_infos "[ResendOperationBlocked] - #{operation[:label]} already sent"
-        @result[:rejected_operation] << operation
-        @result[:rejected_operation_count] += 1
-        next
-      else
-        logger_infos "[ResendOperationBlocked] - #{operation[:label]} sending"
-        @operation = Operation.new(operation)
-        @operation.api_name = 'capidocus'
+      logger_infos "[ResendOperationBlocked] - #{operation[:label]} sending"
+      @operation = Operation.new(operation)
+      @operation.api_name = 'capidocus'
 
-        perform
+      perform(operation)
 
-        @operation.reload
+      @operation.reload
 
-        if @operation.api_name == 'capidocus' && @operation.api_id
-          piece = Pack::Piece.find_by_name(@operation.api_id.gsub("_", ' '))
+      if @operation.api_name == 'capidocus' && @operation.api_id
+        piece = Pack::Piece.find_by_name(@operation.api_id.gsub("_", ' '))
 
-          piece.processed_pre_assignment if piece && !piece.pre_assignment_processed?
-        end
-
-        @operation.user.billing_histories.find_or_create(@operation.date.strftime('%Y%m').to_i, @operation.user.subscription.current_period)
-
-        @result[:created_operation] << operation
-        @result[:created_operation_count] += 1
-
-        logger_infos "[ResendOperationBlocked] - #{operation[:label]} finished"
+        piece.processed_pre_assignment if piece && !piece.pre_assignment_processed?
       end
+
+      @operation.user.billing_histories.find_or_create(@operation.date.strftime('%Y%m').to_i, @operation.user.subscription.current_period)
     end
   end
 
-  def perform
+  def perform(operation)
     set_operation_currency
     set_operation_administrative_infos
 
     append_credit_card_tag if @bank_account.type_name == 'card'
 
+    is_duplicate = is_duplicate?
+    if is_duplicate || @operation.to_lock?
+      @operation.is_locked = true
+
+      if is_duplicate
+        @operation.comment = 'Locked for duplication'
+        logger_infos "[ResendOperationBlocked] - #{operation[:label]} already sent"
+        @result[:rejected_operation] << operation
+        @result[:rejected_operation_count] += 1
+      end
+    end
+
+    if !is_duplicate
+      @result[:created_operation] << operation
+      @result[:created_operation_count] += 1
+
+      logger_infos "[ResendOperationBlocked] - #{operation[:label]} finished"
+    end
+
     @operation.save
 
     @operation
   end
-
 
   def append_credit_card_tag
     @operation.label = '[CB]' + @operation.label
@@ -112,6 +125,10 @@ class PonctualScripts::ResendOperationBlocked < PonctualScripts::PonctualScript
 
   def store_operation_result
     File.write(file_path, JSON.dump(@result))
+  end
+
+  def is_duplicate?
+    @bank_account.operations.where(amount: @operation.amount, date: @operation.date).count > 0
   end
 
   def file_path
