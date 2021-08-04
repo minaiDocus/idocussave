9abb85b0d7ad27ff1e2d5b5898f46f1bc0bfb21d
Mina RAKOTONIRAINY <mina@idocus.com>
2021-08-04 10:07:03 +0300

    Add reinjection method to staffing flows

--- app/models/staffing_flow.rb
+++ app/models/staffing_flow.rb
@@ -12,6 +12,10 @@ class StaffingFlow < ApplicationRecord
   scope :ready_jefacture,           -> { where(kind: 'jefacture', state: 'ready') }
   scope :processing_jefacture,      -> { where(kind: 'jefacture', state: 'processing') }
 
+  scope :grouping,       -> { where(kind: 'grouping') }
+  scope :preassignment,  -> { where(kind: 'preassignment') }
+  scope :jefacture,      -> { where(kind: 'jefacture') }
+
   scope :processed,                 -> { where(state: 'processed') }
 
   state_machine initial: :ready do
@@ -30,5 +34,24 @@ class StaffingFlow < ApplicationRecord
     event :processed do
       transition :processing => :processed
     end
+  end
+
+  def self.reinject(pack_name_list=[], type)
+    count = 0
+
+    Array(pack_name_list).each do |pack_name|
+      staffing = StaffingFlow.where(kind: type).where("params LIKE '%#{pack_name}%'").last
+
+      if staffing
+        p staffing.to_json
+        p "--------------------------------------------------"
+
+        count += 1
+        staffing.state = 'ready'
+        staffing.save
+      end
+    end
+
+    p "Treated #{count} / #{Array(pack_name_list).size}"
   end
 end
