f72543f1f7603c0bc489537461f118892813bf04
Jean <jean@idocus.com>
2021-09-27 11:40:58 +0300

    add conterpart account into vat_accounts journal

--- app/services/ponctual_scripts/migrate_account_book_type_vat_account.rb
+++ app/services/ponctual_scripts/migrate_account_book_type_vat_account.rb
@@ -15,13 +15,15 @@ class PonctualScripts::MigrateAccountBookTypeVatAccount < PonctualScripts::Ponct
     logger_infos "Journals count: #{journals.count}"
 
     journals.each do |journal|
-      vat_account     = journal.vat_account.present? ? journal.vat_account : ''
-      vat_account_10  = journal.vat_account_10.present? ? journal.vat_account_10 : ''
-      vat_account_8_5 = journal.vat_account_8_5.present? ? journal.vat_account_8_5 : ''
-      vat_account_5_5 = journal.vat_account_5_5.present? ? journal.vat_account_5_5 : ''
-      vat_account_2_1 = journal.vat_account_2_1.present? ? journal.vat_account_2_1 : ''
+      raw_journal = JSON.parse(journal.vat_accounts)
 
-      journal.update(vat_accounts: {'0': vat_account, '10': vat_account_10, '8.5': vat_account_8_5, '5.5': vat_account_5_5, '2.1': vat_account_2_1}.to_json)
+      vat_account     = raw_journal['0'] || journal.vat_account.present? ? journal.vat_account : ''
+      vat_account_10  = raw_journal['10'] || journal.vat_account_10.present? ? journal.vat_account_10 : ''
+      vat_account_8_5 = raw_journal['8.5'] || journal.vat_account_8_5.present? ? journal.vat_account_8_5 : ''
+      vat_account_5_5 = raw_journal['5.5'] || journal.vat_account_5_5.present? ? journal.vat_account_5_5 : ''
+      vat_account_2_1 = raw_journal['2.1'] || journal.vat_account_2_1.present? ? journal.vat_account_2_1 : ''
+
+      journal.update(vat_accounts: {'0': [vat_account, ''], '10': [vat_account_10, ''], '8.5': [vat_account_8_5, ''], '5.5': [vat_account_5_5, ''], '2.1': [vat_account_2_1, '']}.to_json)
     end
 
     logger_infos "Migrate AccountBookType vat_accounts done."
